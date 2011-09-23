3d78e1f918e7e6a14e92e8ffeba9abbed8b4d1d9
olonho@google.com <olonho@google.com@c972e689-a6e6-bf8a-7419-c86a4d87c60c>
2011-09-23 20:24:01 +0000

    Simplify making labs.

--- trunk/students/2011/Alexandr.Kartashov/lab1/mymain.cpp
+++ trunk/students/2011/Alexandr.Kartashov/lab1/mymain.cpp
@@ -10,48 +10,6 @@ using namespace std;
 
 // ================================================================================
 
-char* loadFile(const char* file) {
-    int fd = open(file, O_RDONLY);
-    if (fd < 0) {
-        return 0;
-    }
-    struct stat statBuf;
-    if (fstat(fd, &statBuf) != 0) {
-        return 0;
-    }
-
-    off_t size = statBuf.st_size;
-    uint8_t* result = new uint8_t[size + 1];
-    
-    int rv = read(fd, result, size);
-    assert(rv == size);
-    result[size] = '\0';    
-
-    return (char*)result;
-}
-
-void positionToLineOffset(const string& text,
-                          uint32_t position, uint32_t& line, uint32_t& offset) {
-    uint32_t pos = 0, max_pos = text.length();
-    uint32_t current_line = 1, line_start = 0;
-    while (pos < max_pos) {
-        while (pos < max_pos && text[pos] != '\n') {
-            pos++;
-        }
-        if (pos >= position) {
-            line = current_line;
-            offset = position - line_start;
-            return;
-        }
-        assert(text[pos] == '\n' || pos == max_pos);
-        pos++; // Skip newline.
-        line_start = pos;
-        current_line++;
-    }
-}
-
-// --------------------------------------------------------------------------------
-
 int main(int argc, char** argv) {
   Translator* translator = Translator::create();
   const char *expr;
