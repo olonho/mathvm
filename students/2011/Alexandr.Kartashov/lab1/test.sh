6ce9cdf5284cde4b4369851f763d45c7a14628a4
aleksandr.kartashov.aptu.se@gmail.com <aleksandr.kartashov.aptu.se@gmail.com@c972e689-a6e6-bf8a-7419-c86a4d87c60c>
2011-09-24 09:40:36 +0000

    Lab1 refined: generated AST representations are fed back to the program and checked to work

--- students/2011/Alexandr.Kartashov/lab1/test.sh
+++ students/2011/Alexandr.Kartashov/lab1/test.sh
@@ -1,3 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
-for f in $(ls ../../../../tests/*mvm); do echo "Running test using " $f": "; build/bin/lab1 $f; echo; done
+echo "Generaing AST representations..."
+for f in $(ls ../../../../tests/*mvm); do echo "Generating AST from" $f": "; build/bin/lab1 $f > $(basename $f); echo; done
+
+echo "Checking generated representation..."
+
+for f in $(ls *mvm); do echo "Checking" $f": "; build/bin/lab1 $f; echo; done
+rm *mvm
+
+#for f in $(ls ../../../../tests/*mvm); do echo "Running test using " $f": "; build/bin/lab1 $f; echo; done
