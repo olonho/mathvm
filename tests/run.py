779681ff17102013116d72276f46c79c1441f926
olonho@google.com <olonho@google.com@c972e689-a6e6-bf8a-7419-c86a4d87c60c>
2011-10-09 18:54:39 +0000

    Function tests.

--- tests/run.py
+++ tests/run.py
@@ -2,6 +2,8 @@
 
 import subprocess
 import sys
+import os
+import getopt
 
 def runProg(bin, arg):
     pipe = subprocess.Popen([bin, arg],
@@ -13,13 +15,12 @@ def readFile(name):
     f = open(name, 'r')
     return f.read()
 
-mvm = './build/bin/mvm'
 diff = '/usr/bin/diff'
-tests = ['literal', 'add', 'sub', 'mul', 'div', 'expr', 'for', 'while', 'if'];
+tests = ['literal', 'add', 'sub', 'mul', 'div', 'expr', 'for', 'while', 'if', 'function'];
 
-def runTest(test):
-    result = runProg(mvm, './tests/'+test+'.mvm')
-    expectFile = './tests/'+test+'.expect'
+def runTest(mvm, root, test):
+    result = runProg(mvm, os.path.join(root, test+'.mvm'))
+    expectFile = os.path.join(root, test+'.expect')
     expect = readFile(expectFile)
     if (expect == result):
         print 'Test "'+test+'" has PASSED'
@@ -36,11 +37,36 @@ def runTest(test):
                             stdin=subprocess.PIPE, stdout=subprocess.PIPE)
         (out, err) = pipe.communicate(result)
         print out
-        
+
 
 def main(argv):
-    for t in tests:
-        runTest(t)
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'a:k:', ['add=', 'kind='])
+        add_dir = None
+        kind = 'debug'
+        for o, a in opts:
+            if o == '-a':
+              add_dir = a
+            if o =='-k':
+              kind = a
+        mvm = os.path.join('./build', kind, 'mvm')
+        if add_dir is not None:
+          import glob
+          import re
+          add_tests = glob.glob(os.path.join(add_dir, '*.mvm'))
+          for tp in add_tests:
+              m = re.search(r"([\w]+)\.mvm", tp)
+              if m is None:
+                  continue
+              t = m.group(1)
+              runTest(mvm, add_dir, t)
+        for t in tests:
+            runTest(mvm, './tests', t)
+    except getopt.GetoptError, err:
+        # print help information and exit:
+        print str(err)
+        print "A"
+        sys.exit(2)
 
 if __name__ == '__main__':
     main(sys.argv)
