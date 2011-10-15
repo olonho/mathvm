d95d4a9eeaf848e6ed5a188e394da31010e730be
MariyaFomkina <MariyaFomkina@c972e689-a6e6-bf8a-7419-c86a4d87c60c>
2011-10-15 12:06:48 +0000

    Initial version of interpreter (no function, no action, just updated bytecode translator)

---
+++ students/2011/Maria.Fomkina/03.interpreter/test.sh
@@ -0,0 +1,25 @@
+MVM=./build/debug/mvm
+TEST_FILES=../../../../tests
+
+echo "**************Testing is started!*************"
+
+echo "**************Default*************************"
+echo "======= Original ======="
+echo "double x; double y;"
+echo "x += 8.0; y = 2.0;" 
+echo "print('Hello, x=',x,' y=',y,'\n');"
+echo "===== Translation ======"
+$MVM
+
+for t in $TEST_FILES/*.mvm 
+do
+echo
+echo "**************Testing file "$t"***************"
+echo "======= Original ======="
+cat $t
+echo "===== Translation ======"
+$MVM $t
+done
+
+echo
+echo "**************Testing is finished! ************"
