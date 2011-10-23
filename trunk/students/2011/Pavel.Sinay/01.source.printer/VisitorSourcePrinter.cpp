03f438116c5262464fbd9b2e7853000854eae612
pavel.sinay <pavel.sinay@c972e689-a6e6-bf8a-7419-c86a4d87c60c>
2011-10-23 13:51:55 +0000

    First lab done

--- trunk/students/2011/Pavel.Sinay/01.source.printer/VisitorSourcePrinter.cpp
+++ trunk/students/2011/Pavel.Sinay/01.source.printer/VisitorSourcePrinter.cpp
@@ -5,12 +5,11 @@
  *      Author: niea
  */
 
-#include <iostream>
 #include "VisitorSourcePrinter.h"
 
-VisitorSourcePrinter::VisitorSourcePrinter() {
+VisitorSourcePrinter::VisitorSourcePrinter(std::ostream &msg_stream) :
+	m_stream(msg_stream) {
 	// TODO Auto-generated constructor stub
-
 }
 
 VisitorSourcePrinter::~VisitorSourcePrinter() {
@@ -18,47 +17,131 @@ VisitorSourcePrinter::~VisitorSourcePrinter() {
 }
 
 void VisitorSourcePrinter::visitIfNode(mathvm::IfNode *node) {
+	//m_stream << "<if node> ";
+	m_stream << "if (";
+	node->ifExpr()->visit(this);
+	m_stream << ") {" << std::endl;
+	node->thenBlock()->visit(this);
 
+	if (node->elseBlock()) {
+		m_stream << std::endl << "} else {" << std::endl;
+		node->elseBlock()->visit(this);
+	}
+	m_stream << "}" << std::endl;
 }
 
 void VisitorSourcePrinter::visitPrintNode(mathvm::PrintNode *node) {
-	std::cout << "print node" << std::endl;
+	//m_stream << "print node" << std::endl;
+	m_stream << "print (";
+	//node->visitChildren(this);
+	for (unsigned int i = 0; i < node->operands(); ++i) {
+		node->operandAt(i)->visit(this);
+		if (i != node->operands() - 1) {
+			m_stream << ", ";
+		}
+	}
+
+	m_stream << ");" << std::endl;
 }
 
 void VisitorSourcePrinter::visitLoadNode(mathvm::LoadNode *node) {
+	//m_stream << "<load node> ";
+	m_stream << node->var()->name();
 }
 
 void VisitorSourcePrinter::visitForNode(mathvm::ForNode *node) {
+	//m_stream << "<for node> ";
+	m_stream << "for (" << node->var()->name() << " in ";
+	node->inExpr()->visit(this);
+	m_stream << ") {" << std::endl;
+	node->body()->visit(this);
+	m_stream << "}" << std::endl;
 }
 
 void VisitorSourcePrinter::visitIntLiteralNode(mathvm::IntLiteralNode *node) {
+	//m_stream << "<IntLiteral node> ";
+	m_stream << node->literal();
 }
 
 void VisitorSourcePrinter::visitUnaryOpNode(mathvm::UnaryOpNode *node) {
+	//m_stream << "<unary op node> ";
+	m_stream << tokenOp(node->kind());
+	node->operand()->visit(this);
 }
 
 void VisitorSourcePrinter::visitFunctionNode(mathvm::FunctionNode *node) {
+	//m_stream << "<function node> " << std::endl;
+	node->visitChildren(this);
 }
 
 void VisitorSourcePrinter::visitWhileNode(mathvm::WhileNode *node) {
+	//m_stream << "<while node> ";
+	m_stream << "while (";
+	node->whileExpr()->visit(this);
+	m_stream << ") {" << std::endl;
+	node->loopBlock()->visit(this);
+	m_stream << std::endl << "}" << std::endl;
 }
 
 void VisitorSourcePrinter::visitBinaryOpNode(mathvm::BinaryOpNode *node) {
+	//m_stream << "<binary op node> ";
+	m_stream << "(";
+	node->left()->visit(this);
+	m_stream << tokenOp(node->kind());
+	node->right()->visit(this);
+	m_stream << ")";
 }
 
 void VisitorSourcePrinter::visitBlockNode(mathvm::BlockNode *node) {
+	//m_stream << "<block node> " << std::endl;
+	mathvm::Scope::VarIterator it(node->scope());
+	while (it.hasNext()) {
+		mathvm::AstVar *var = it.next();
+		m_stream << varTypeToStr(var->type()) << " " << var->name() << ";"
+				<< std::endl;
+	}
 	node->visitChildren(this);
-	std::cout << "block node" << std::endl;
 }
 
 void VisitorSourcePrinter::visitDoubleLiteralNode(
 		mathvm::DoubleLiteralNode *node) {
+	//m_stream << "<double literal node> ";
+	m_stream << node->literal();
 }
 
 void VisitorSourcePrinter::visitStringLiteralNode(
 		mathvm::StringLiteralNode *node) {
+	//m_stream << "<string literal node> ";
+	std::string str = node->literal();
+	int n_pos;
+	while ((n_pos = str.find('\n')) >= 0) {
+		str.erase(n_pos, 1);
+		str.insert(n_pos, "\\n");
+	}
+	m_stream << "'" << str << "'";
 }
 
 void VisitorSourcePrinter::visitStoreNode(mathvm::StoreNode *node) {
+	//m_stream << "<store node> ";
+	//node->visitChildren(this);
+	m_stream << node->var()->name() << tokenOp(node->op());
+	node->value()->visit(this);
+	m_stream << ';' << std::endl;
+}
+
+std::string VisitorSourcePrinter::varTypeToStr(mathvm::VarType type) {
+	switch (type) {
+	case mathvm::VT_INVALID:
+		return "<invalid var type>";
+	case mathvm::VT_VOID:
+		return "void";
+	case mathvm::VT_DOUBLE:
+		return "double";
+	case mathvm::VT_INT:
+		return "int";
+	case mathvm::VT_STRING:
+		return "string";
+	}
+	return "";
 }
 
