e106106aa3ebc5cf363a20abb9dbbce60e805837
olonho@google.com <olonho@google.com@c972e689-a6e6-bf8a-7419-c86a4d87c60c>
2011-10-29 09:21:56 +0000

    Fix build on some gcc.

--- trunk/libs/asmjit/Util.h
+++ trunk/libs/asmjit/Util.h
@@ -109,7 +109,7 @@ static inline uint32_t bitCount(uint32_t x)
 {
   x = x - ((x >> 1) & 0x55555555);
   x = (x & 0x33333333) + ((x >> 2) & 0x33333333);
-  return ((x + (x >> 4) & 0xF0F0F0F) * 0x1010101) >> 24;
+  return (((x + (x >> 4)) & 0xF0F0F0F) * 0x1010101) >> 24;
 }
 
 static inline uint32_t findFirstBit(uint32_t mask) ASMJIT_NOTHROW
