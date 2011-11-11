ffc821602530dc071cda00b85b3cb8a6c9819cd7
eabatalov89 <eabatalov89@c972e689-a6e6-bf8a-7419-c86a4d87c60c>
2011-11-11 15:27:20 +0000

    rebuilded all and found one more compiltion error, fixed

--- trunk/students/2011/Evgeny.Batalov/vm/ByteCodeAnnotations.h
+++ trunk/students/2011/Evgeny.Batalov/vm/ByteCodeAnnotations.h
@@ -1,7 +1,8 @@
 #pragma once
 #include <mathvm.h>
 
-typedef enum {
-  BE_FCALL_BEGIN = 0, //Begin of code to call func. Code includes pushing of func parameters, closures and locals on stack
+typedef enum { 
+  //Begin of code to call func. Code includes pushing of func parameters, closures and locals on stack
+  BE_FCALL_BEGIN = static_cast<int>(mathvm::BC_LAST),
   BE_FCALL_END
 } BytecodeExts;
