43dd3d03a3701a73d1a8e4a4c829aef591f40117
alexeigurevich@gmail.com <alexeigurevich@gmail.com@c972e689-a6e6-bf8a-7419-c86a4d87c60c>
2012-01-14 17:54:54 +0000

    lab1.printer added (with native functions support)

---
+++ students/2011/Alexey.Gurevich/lab1.printer/printer.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "ast.h"
+
+class Printer : public mathvm::AstVisitor {
+	virtual void visitBinaryOpNode(mathvm::BinaryOpNode* node);
+	virtual void visitUnaryOpNode(mathvm::UnaryOpNode* node);
+	virtual void visitStringLiteralNode(mathvm::StringLiteralNode* node);
+	virtual void visitDoubleLiteralNode(mathvm::DoubleLiteralNode* node);
+	virtual void visitIntLiteralNode(mathvm::IntLiteralNode* node);
+	virtual void visitLoadNode(mathvm::LoadNode* node);
+	virtual void visitStoreNode(mathvm::StoreNode* node);
+	virtual void visitBlockNode(mathvm::BlockNode* node);
+	virtual void visitNativeCallNode(mathvm::NativeCallNode* node); // TODO
+	virtual void visitForNode(mathvm::ForNode* node);
+	virtual void visitWhileNode(mathvm::WhileNode* node);
+	virtual void visitIfNode(mathvm::IfNode* node);
+	virtual void visitReturnNode(mathvm::ReturnNode* node); // TODO
+	virtual void visitFunctionNode(mathvm::FunctionNode* node);
+	virtual void visitCallNode(mathvm::CallNode* node);
+	virtual void visitPrintNode(mathvm::PrintNode* node);
+
+	// auxiliary
+	static std::string varTypeToString(mathvm::VarType varType);
+
+public:
+	Printer();
+
+private:
+	// for correct Functions calls printing
+	bool isSemicolonNeeded;
+};
