45b943225e9eba072e86ffa763d241d5b277e4ba
AM5800@gmail.com <AM5800@gmail.com@c972e689-a6e6-bf8a-7419-c86a4d87c60c>
2012-01-14 12:07:26 +0000

    Who cares what am I committing?

---
+++ trunk/students/2011/Alexandr.Mikhajlov/mathvm.Tests/DefaultTests.cs
@@ -0,0 +1,67 @@
+using NUnit.Framework;
+
+namespace mathvm.Tests
+{
+	[TestFixture]
+	internal class DefaultTests : BaseTest
+	{
+		[Test]
+		public void DivTest()
+		{
+			PerformTest("div");
+		}
+
+		[Test]
+		public void AddTest()
+		{
+			PerformTest("add");
+		}
+
+		[Test]
+		public void ForTest()
+		{
+			PerformTest("for");
+		}
+
+		[Test]
+		public void FunctionTest()
+		{
+			PerformTest("function");
+		}
+
+		[Test]
+		public void IfTest()
+		{
+			PerformTest("if");
+		}
+
+		[Test]
+		public void LiteralTest()
+		{
+			PerformTest("literal");
+		}
+
+		[Test]
+		public void MulTest()
+		{
+			PerformTest("mul");
+		}
+
+		[Test]
+		public void ExprTest()
+		{
+			PerformTest("expr");
+		}
+
+		[Test]
+		public void SubTest()
+		{
+			PerformTest("sub");
+		}
+		[Test]
+		public void WhileTest()
+		{
+			PerformTest("while");
+		}
+	}
+}
