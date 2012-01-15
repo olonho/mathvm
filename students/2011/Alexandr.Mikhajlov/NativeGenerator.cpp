f1823c1acf8ac48472296c96ccb6e252b3b548d4
AM5800@gmail.com <AM5800@gmail.com@c972e689-a6e6-bf8a-7419-c86a4d87c60c>
2012-01-15 18:44:25 +0000

    simplest arithmetics done

--- students/2011/Alexandr.Mikhajlov/NativeGenerator.cpp
+++ students/2011/Alexandr.Mikhajlov/NativeGenerator.cpp
@@ -1,5 +1,7 @@
 #include "NativeGenerator.h"
 #include <iostream>
+#include <string>
+#include <sstream>
 
 #include "asmjit\Build.h"
 #include "AsmJit/Assembler.h"
@@ -20,10 +22,30 @@ void PrintString(int64_t address) {
 	printf("%s", s->c_str());
 }
 
+void PrintDouble( double value )
+{
+	std::stringstream stream;
+	stream << value;
+	std::string s = stream.str();
+	int i = s.find("e+0");
+	if (i != -1) {
+		s.replace(i, 3, "e+");
+	}
+
+	i = s.find("e-0");
+	if (i != -1) {
+		s.replace(i, 3, "e-");
+	}
+
+  std::cout << s;
+}
+
+
 void NativeGenerator::Compile( mathvm::AstFunction * rootNode)
 {
 	myFirstPassVisitor.visit(rootNode);
-	
+	myResultVar.Integer = NULL;
+
 	myCompiler = new Compiler;
 	
 	FileLogger logger(stderr);
@@ -31,6 +53,8 @@ void NativeGenerator::Compile( mathvm::AstFunction * rootNode)
 
 	myCompiler->newFunction(CALL_CONV_DEFAULT, FunctionBuilder0<void>());
 	myCompiler->getFunction()->setHint(FUNCTION_HINT_NAKED, true);
+
+	CreateAsmVar(mathvm::VT_INT);
 
 	rootNode->node()->body()->visit(this);
 	
@@ -45,29 +69,54 @@ void NativeGenerator::Compile( mathvm::AstFunction * rootNode)
 
 void NativeGenerator::visitPrintNode(mathvm::PrintNode* node) 
 {
-	GPVar v(myCompiler->newGP());
-	myVars.push(v);
+	AsmVarPtr old = myResultVar;
 	for (unsigned int i = 0; i < node->operands(); ++i) {
     AstNode* op = node->operandAt(i);
-    op->visit(this);
-		void * fptr = NULL;
+    
+		myResultVar = CreateAsmVar(GetNodeType(op));
+		op->visit(this);
+
+		ECall* ctx = NULL;
+
 		switch (GetNodeType(op)) {
-		case mathvm::VT_INT: fptr = PrintInt;break;
-		case mathvm::VT_STRING: fptr = PrintString;break;
+		case mathvm::VT_INT: 
+			ctx = myCompiler->call(PrintInt);
+			ctx->setPrototype(CALL_CONV_DEFAULT, FunctionBuilder1<void, int64_t>());
+			ctx->setArgument(0, *myResultVar.Integer);
+			break;
+
+		case mathvm::VT_STRING: 
+			ctx = myCompiler->call(PrintString);
+			ctx->setPrototype(CALL_CONV_DEFAULT, FunctionBuilder1<void, int64_t>());
+			ctx->setArgument(0, *myResultVar.Integer);
+			break;
+
+		case mathvm::VT_DOUBLE: 
+			ctx = myCompiler->call(PrintDouble);
+			ctx->setPrototype(CALL_CONV_DEFAULT, FunctionBuilder1<void, double>());
+			ctx->setArgument(0, *myResultVar.Double);
+			break;
 		}
-
-		GPVar address(myCompiler->newGP());
-		myCompiler->mov(address, imm((sysint_t)(void*)fptr));
-
-		ECall* ctx = myCompiler->call(fptr);
-    ctx->setPrototype(CALL_CONV_DEFAULT, FunctionBuilder1<void, int64_t>());
-	  ctx->setArgument(0, v);
   }
+
+	myResultVar = old;
 }
+
+AsmVarPtr NativeGenerator::CreateAsmVar( mathvm::VarType type )
+{
+	AsmVarPtr p;
+	switch(type) {
+	case VT_DOUBLE: p.Double = new XMMVar(myCompiler->newXMM()); break;
+	default:
+		p.Integer = new GPVar(myCompiler->newGP());
+	}
+	return p;
+}
+
 
 void NativeGenerator::visitIntLiteralNode(mathvm::IntLiteralNode* node) 
 {
-	myCompiler->mov(myVars.top(), node->literal());
+	myCompiler->mov(*myResultVar.Integer, node->literal());
 }
 
 void NativeGenerator::visitBlockNode(mathvm::BlockNode* node)
@@ -80,9 +129,122 @@ void NativeGenerator::visitStringLiteralNode(mathvm::StringLiteralNode* node)
 	uint16_t id = myCode.makeStringConstant(node->literal());
 	std::string const & s = myCode.constantById(id);
 	int64_t p = (int64_t)(void*)&s;
-	myCompiler->mov(myVars.top(), p);
+	myCompiler->mov(*myResultVar.Integer, p);
 }
 
+void NativeGenerator::visitDoubleLiteralNode(mathvm::DoubleLiteralNode* node)
+{
+	GPVar v(myCompiler->newGP());
+	double value = node->literal();
+	int64_t nvalue = *((int64_t*) &value);
+	
+	myCompiler->mov(v, imm(nvalue));
+	myCompiler->movq(*myResultVar.Double, v);
+	myCompiler->unuse(v);
+}
 
+void NativeGenerator::visitBinaryOpNode(mathvm::BinaryOpNode* node)
+{
+  AsmVarPtr old = myResultVar;
 
+	VarType expectedType = GetNodeType(node);
+	
+	myResultVar = CreateAsmVar(expectedType);
+	AsmVarPtr left = VisitWithTypeControl(node->left(), expectedType);
+	
+	myResultVar = CreateAsmVar(expectedType);
+	AsmVarPtr right = VisitWithTypeControl(node->right(), expectedType);
+
+	myResultVar = old;
+	if (myResultVar.Integer == NULL) return;
+
+	if (TryDoArithmetics(node, left, right, expectedType)) return;
+}
+
+AsmVarPtr NativeGenerator::VisitWithTypeControl( mathvm::AstNode * node, mathvm::VarType expectedType )
+{
+	mathvm::VarType type = GetNodeType(node);
+	if (expectedType == VT_STRING || type == VT_STRING) throw new std::exception("Binary operations for strings not supported");
+	
+	node->visit(this);
+
+	if (expectedType != type) {
+		AsmVarPtr nw = AsmVarPtr();
+
+		if (expectedType == VT_DOUBLE) {
+			nw.Double = new XMMVar(myCompiler->newXMM());
+
+			myCompiler->cvtsi2sd(*nw.Double, *myResultVar.Integer );
+			return nw;
+		}
+		if (expectedType == mathvm::VT_INT) {
+			nw.Integer = new GPVar(myCompiler->newGP());
+			myCompiler->cvtsd2si(*nw.Integer, *myResultVar.Double);
+		}
+	}
+	return myResultVar;
+}
+
+int64_t idiv(int64_t a, int64_t b) {
+	return a / b;
+
+}
+
+bool NativeGenerator::TryDoArithmetics( mathvm::BinaryOpNode* node, AsmVarPtr left, AsmVarPtr right, mathvm::VarType expectedType )
+{
+	switch(node->kind()) {
+	case tADD: 
+		if (expectedType == mathvm::VT_INT) {
+			myCompiler->mov(*myResultVar.Integer, *left.Integer);
+			myCompiler->add(*myResultVar.Integer, *right.Integer);
+			return true;
+		}
+		else if (expectedType == mathvm::VT_DOUBLE) {
+			myCompiler->movq(*myResultVar.Double, *left.Double);
+			myCompiler->addsd(*myResultVar.Double, *right.Double);
+			return true;
+		}
+		break;
+	case tMUL:
+		if (expectedType == mathvm::VT_INT) {
+			myCompiler->mov(*myResultVar.Integer, *left.Integer);
+			myCompiler->imul(*myResultVar.Integer, *right.Integer);
+			return true;
+		}
+		else if (expectedType == mathvm::VT_DOUBLE) {
+			myCompiler->movq(*myResultVar.Double, *left.Double);
+			myCompiler->mulsd(*myResultVar.Double, *right.Double);
+			return true;
+		}
+		break;
+	case tSUB:
+		if (expectedType == mathvm::VT_INT) {
+			myCompiler->mov(*myResultVar.Integer, *left.Integer);
+			myCompiler->sub(*myResultVar.Integer, *right.Integer);
+			return true;
+		}
+		else if (expectedType == mathvm::VT_DOUBLE) {
+			myCompiler->movq(*myResultVar.Double, *left.Double);
+			myCompiler->subsd(*myResultVar.Double, *right.Double);
+			return true;
+		}
+		break;
+	case tDIV:
+		if (expectedType == mathvm::VT_INT) {
+			ECall * ctx = myCompiler->call(imm((size_t)idiv));
+			ctx->setPrototype(CALL_CONV_DEFAULT, FunctionBuilder2<int64_t,int64_t,int64_t>());
+			ctx->setArgument(0, *left.Integer);
+			ctx->setArgument(1, *right.Integer);
+			ctx->setReturn(*myResultVar.Integer);
+			return true;
+		}
+		else if (expectedType == mathvm::VT_DOUBLE) {
+			myCompiler->movq(*myResultVar.Double, *left.Double);
+			myCompiler->divsd(*myResultVar.Double, *right.Double);
+			return true;
+		}
+		break;
+	}
+	return false;
+}
 
