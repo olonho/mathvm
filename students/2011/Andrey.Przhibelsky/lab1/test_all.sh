5322eeea6e67ea83aa3eecaa5bd6958d7283582f
andrewprzh@gmail.com <andrewprzh@gmail.com@c972e689-a6e6-bf8a-7419-c86a4d87c60c>
2012-01-23 23:28:38 +0000

    lab 1 microfix

--- students/2011/Andrey.Przhibelsky/lab1/test_all.sh
+++ students/2011/Andrey.Przhibelsky/lab1/test_all.sh
@@ -11,4 +11,13 @@ do
     cmp  tests/$(basename $file) tests2/$(basename $file)
 done
 
+
+for file in $(ls ../../../../tests/*tional/*.mvm)
+do 
+    build/debug/lab1 $file tests/$(basename $file)
+    build/debug/lab1 tests/$(basename $file) tests2/$(basename $file)
+    
+    cmp  tests/$(basename $file) tests2/$(basename $file)
+done
+
 rm -rf tests tests2
