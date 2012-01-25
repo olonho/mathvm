88754cab45b9d93bf5d598753b939710b278bed2
AM5800@gmail.com <AM5800@gmail.com@c972e689-a6e6-bf8a-7419-c86a4d87c60c>
2012-01-25 13:15:17 +0000

    casts.mvm passed

--- students/2011/Alexandr.Mikhajlov/mathvm.Tests/AdditionalTests.cs
+++ students/2011/Alexandr.Mikhajlov/mathvm.Tests/AdditionalTests.cs
@@ -7,13 +7,73 @@ namespace mathvm.Tests
 	{
 		public override string TestsDirectory
 		{
-			get { return Path.Combine(VmRoot, @"additional\tests"); }
+			get { return Path.Combine(VmRoot, @"tests\additional"); }
 		}
 
 		[Test]
 		public void FibClosure()
 		{
 			PerformTest("fib_closure");
+		}
+
+		[Test]
+		public void Ackermann()
+		{
+			PerformTest("ackermann");
+		}
+
+		[Test]
+		public void AckermannClosure()
+		{
+			PerformTest("ackermann_closure");
+		}
+
+		[Test]
+		public void Casts()
+		{
+			PerformTest("casts");
+		}
+
+		[Test]
+		public void Complex()
+		{
+			PerformTest("complex");
+		}
+
+		[Test]
+		public void Complex2()
+		{
+			PerformTest("complex2");
+		}
+
+		[Test]
+		public void Fib()
+		{
+			PerformTest("fib");
+		}
+
+		[Test]
+		public void Function()
+		{
+			PerformTest("function");
+		}
+
+		[Test]
+		public void FunctionCall()
+		{
+			PerformTest("function-call");
+		}
+
+		[Test]
+		public void FunctionCast()
+		{
+			PerformTest("function-cast");
+		}
+
+		[Test]
+		public void Vars()
+		{
+			PerformTest("vars");
 		}
 	}
 }
