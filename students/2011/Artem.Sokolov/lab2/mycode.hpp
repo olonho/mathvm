2bf0ced75a541dc61e559be87d20718718f73cae
Artem.A.Sokolov@gmail.com <Artem.A.Sokolov@gmail.com@c972e689-a6e6-bf8a-7419-c86a4d87c60c>
2012-02-01 19:53:49 +0000

    Lab3 work in progress.

--- students/2011/Artem.Sokolov/lab2/mycode.hpp
+++ students/2011/Artem.Sokolov/lab2/mycode.hpp
@@ -16,13 +16,15 @@ union StackFrameUnit {
 };
 
 class MyCode: public mathvm::Code {
-	StackFrameUnit *current_unit;
+	StackFrameUnit *current_frame;
 	StackFrameUnit *call_stack;
 
 	Variable *stack;
 	Variable *top;
 
-	uint16_t current_context;
+	uint16_t current_context_id;
+
+	StackFrameUnit *frame_top;
 
 	void push_int(int64_t value) {
 		top->i = value;
@@ -39,6 +41,11 @@ class MyCode: public mathvm::Code {
 		++top;
 	}
 
+	void push_string(char const *string) {
+		top->s = string;
+		++top;
+	}
+
 	int64_t pop_int() {
 		--top;
 		return top->i;
@@ -52,6 +59,14 @@ class MyCode: public mathvm::Code {
 	char const *pop_string() {
 		--top;
 		return top->s;
+	}
+
+	StackFrameUnit *get_frame(uint16_t context_id) {
+		StackFrameUnit *frame = current_frame;
+		assert(context_id <= current_context_id);
+		for (int i = current_context_id - context_id; i > 0; ++i);
+			frame = frame[-1].previous;
+		return frame;
 	}
 
 	mathvm::Status *execute_bytecode(mathvm::Bytecode *bytecode) {
@@ -92,6 +107,80 @@ class MyCode: public mathvm::Code {
 		    	push_string(bytecode->getInt16(index));
 		    	index += 2;
 		    	break;
+
+		    case mathvm::BC_STORECTXIVAR: {
+		    	uint16_t context_id = bytecode->getInt16(index);
+		    	index += 2;
+		    	uint16_t id = bytecode->getInt16(index);
+		    	index += 2;
+
+		    	StackFrameUnit *frame;
+		    	if (current_context_id == context_id) {
+		    		frame_top += id + 1;
+		    		frame = current_frame;
+		    	} else {
+		    		frame = get_frame(context_id);
+		    	}
+
+		    	frame[id].v.i = pop_int();
+		    }   break;
+		    case mathvm::BC_STORECTXDVAR: {
+		    	uint16_t context_id = bytecode->getInt16(index);
+				index += 2;
+				uint16_t id = bytecode->getInt16(index);
+				index += 2;
+
+		    	StackFrameUnit *frame;
+		    	if (current_context_id == context_id) {
+		    		frame_top += id + 1;
+		    		frame = current_frame;
+		    	} else {
+		    		frame = get_frame(context_id);
+		    	}
+
+				frame[id].v.d = pop_double();
+		    }   break;
+		    case mathvm::BC_STORECTXSVAR: {
+				uint16_t context_id = bytecode->getInt16(index);
+				index += 2;
+				uint16_t id = bytecode->getInt16(index);
+				index += 2;
+
+		    	StackFrameUnit *frame;
+		    	if (current_context_id == context_id) {
+		    		frame_top += id + 1;
+		    		frame = current_frame;
+		    	} else {
+		    		frame = get_frame(context_id);
+		    	}
+
+				frame[id].v.s = pop_string();
+			}   break;
+		    case mathvm::BC_LOADCTXSVAR: {
+				uint16_t context_id = bytecode->getInt16(index);
+				index += 2;
+				uint16_t id = bytecode->getInt16(index);
+				index += 2;
+				StackFrameUnit *frame = get_frame(context_id);
+				push_string(frame[id].v.s);
+			}   break;
+		    case mathvm::BC_LOADCTXDVAR: {
+				uint16_t context_id = bytecode->getInt16(index);
+				index += 2;
+				uint16_t id = bytecode->getInt16(index);
+				index += 2;
+				StackFrameUnit *frame = get_frame(context_id);
+				push_double(frame[id].v.d);
+			}   break;
+		    case mathvm::BC_LOADCTXIVAR: {
+				uint16_t context_id = bytecode->getInt16(index);
+				index += 2;
+				uint16_t id = bytecode->getInt16(index);
+				index += 2;
+				StackFrameUnit *frame = get_frame(context_id);
+				push_int(frame[id].v.i);
+			}   break;
+
 		    case mathvm::BC_IADD:
 				push_int(pop_int() + pop_int());
 				break;
@@ -122,12 +211,14 @@ class MyCode: public mathvm::Code {
 		    case mathvm::BC_DNEG:
 				push_int(-pop_double());
 				break;
+
 		    case mathvm::BC_I2D:
 				top->d = (double)top->i;
 				break;
 		    case mathvm::BC_D2I:
 				top->i = (int)top->d;
 				break;
+
 		    case mathvm::BC_IPRINT:
 				std::cout << pop_int();
 				break;
@@ -137,6 +228,23 @@ class MyCode: public mathvm::Code {
 		    case mathvm::BC_SPRINT:
 		    	std::cout << pop_string();
 				break;
+
+		    case mathvm::BC_CALL: {
+		    	uint16_t function_id = bytecode->getInt16(index);
+				index += 2;
+
+				frame_top->previous = current_frame;
+				frame_top += 1;
+				current_frame = frame_top;
+
+		    	mathvm::BytecodeFunction *function = (mathvm::BytecodeFunction *) functionById(function_id);
+				execute_bytecode(function->bytecode());
+		    }	break;
+			case mathvm::BC_RETURN: {
+				frame_top = current_frame - 1;
+				current_frame = current_frame[-1].previous;
+			}	break;
+
 		    case mathvm::BC_JA:
 				index += bytecode->getInt16(index);
 				break;
@@ -166,6 +274,7 @@ class MyCode: public mathvm::Code {
 				break;
 		    case mathvm::BC_STOP:
 		    	return NULL;
+		    	break;
 		    default:
 				assert(false);
 				break;
@@ -177,12 +286,13 @@ class MyCode: public mathvm::Code {
 public:
 	MyCode() {
 		call_stack = (StackFrameUnit *)std::malloc(sizeof(StackFrameUnit) * 8192);
-		current_unit = call_stack;
+		current_frame = call_stack + 1;
+		frame_top = current_frame;
 
 		stack = (Variable *)std::malloc(sizeof(Variable) * 1024);
 		top = stack;
 
-		current_context = 0;
+		current_context_id = 0;
 	}
 
 	~MyCode() {
@@ -192,6 +302,7 @@ public:
 
 	mathvm::Status *execute(std::vector<mathvm::Var*> &vars) {
 		mathvm::BytecodeFunction *top = (mathvm::BytecodeFunction *) functionById(0);
+		current_frame[-1].previous = NULL;
 		return execute_bytecode(top->bytecode());
 	}
 };
