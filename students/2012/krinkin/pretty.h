c17c7db376cbb83c5cbc8368e27aa3fa69113ee3
krinkin.m.u@gmail.com <krinkin.m.u@gmail.com@c972e689-a6e6-bf8a-7419-c86a4d87c60c>
2012-09-19 22:41:44 +0000

    krinkin, ast print

---
+++ students/2012/krinkin/pretty.h
@@ -0,0 +1,45 @@
+#ifndef __PRETTY_PRINTER_H__
+#define __PRETTY_PRINTER_H__
+
+#include <iterator>
+#include <string>
+
+#include "mathvm.h"
+#include "ast.h"
+
+using namespace mathvm;
+
+class PrettyPrinter : public AstVisitor
+{
+public:
+	virtual void visitTopLevelBlock(AstFunction* top);
+	virtual void visitBinaryOpNode(BinaryOpNode *node);
+	virtual void visitUnaryOpNode(UnaryOpNode *node);
+	virtual void visitStringLiteralNode(StringLiteralNode *node);
+	virtual void visitDoubleLiteralNode(DoubleLiteralNode *node);
+	virtual void visitIntLiteralNode(IntLiteralNode *node);
+	virtual void visitLoadNode(LoadNode *node);
+	virtual void visitStoreNode(StoreNode *node);
+	virtual void visitForNode(ForNode *node);
+	virtual void visitWhileNode(WhileNode *node);
+	virtual void visitIfNode(IfNode *node);
+	virtual void visitBlockNode(BlockNode *node);
+	virtual void visitFunctionNode(FunctionNode *node);
+	virtual void visitReturnNode(ReturnNode *node);
+	virtual void visitCallNode(CallNode *node);
+	virtual void visitNativeCallNode(NativeCallNode *node);
+	virtual void visitPrintNode(PrintNode *node);
+
+	PrettyPrinter(std::ostream &out): m_indent(0), m_out(out) {}
+	virtual ~PrettyPrinter() {}
+
+private:
+	size_t m_indent;
+	std::ostream &m_out;
+	
+	std::string typeStr(VarType type);
+	void printBlock(BlockNode *node);
+	std::string escape(std::string const & str);
+};
+
+#endif /* __PRETTY_PRINTER_H__ */
