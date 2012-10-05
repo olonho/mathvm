0d348e5fd870cd89d3ef4192a87056ca2dd67f8f
gmpota@gmail.com <gmpota@gmail.com@c972e689-a6e6-bf8a-7419-c86a4d87c60c>
2012-10-05 00:11:20 +0000

    init

---
+++ students/2012/velikiy2/astView/main.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+
+#include "mathvm.h"
+#include "main.h"
+#include "printastvisitor.h"
+
+using namespace mathvm;
+using namespace std;
+
+int main(int argc, char** argv) {
+
+    const char* programSourceFile = "function.mvm";
+    const char* expr = loadFile(programSourceFile);
+    
+    if (expr == 0) {
+        cerr << "Cannot read file: " << programSourceFile << endl;
+        return 1;
+    }
+    
+    Parser parser;
+    Status* status = parser.parseProgram(expr);
+    if(status != NULL && status->isError()) {
+        uint32_t position = status->getPosition();
+        uint32_t line = 0, offset = 0;
+        positionToLineOffset(expr, position, line, offset);
+        cerr << "Cannot translate expression: expression at "
+             << line << ", " << offset << "; " << endl
+             << "error " 
+             << status->getError().c_str() << endl;
+        delete[] expr;
+        return 2;
+        
+    }
+    
+    showFormatedCode(parser.top());
+
+    delete[] expr;
+    
+    return 0;
+    
+}
+
+void showFormatedCode(const mathvm::AstFunction* rootFunction){
+    
+    PrintAstVisitor printVisitor;
+    
+    BlockNode* node = rootFunction->node()->body();
+    
+    for(uint32_t i = 0; i < node->nodes(); i++) {
+        node->nodeAt(i)->visit(&printVisitor);
+        cout << ";" << endl;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
