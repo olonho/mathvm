917021cd2a64332f52a13901165abfb002de763e
sergey.kazenyuk@gmail.com <sergey.kazenyuk@gmail.com@c972e689-a6e6-bf8a-7419-c86a4d87c60c>
2012-11-20 19:09:03 +0000

    Fix jump offset calculation.

--- trunk/students/2012/kazenyuk/ast2bytecode/BytecodeInstructionPrimitives.h
+++ trunk/students/2012/kazenyuk/ast2bytecode/BytecodeInstructionPrimitives.h
@@ -38,37 +38,40 @@ public:
 //                                  << std::endl;
 
 //                out->addBranch(BC_IFICMPG, *(new Label()));
-        out->addInsn(BC_IFICMPE);
-        Cmp(out, leftType, rightType);
+        Cmp(out, BC_IFICMPE, leftType, rightType);
     }
 
     void CmpNeq(Bytecode* out, VarType leftType = VT_VOID, VarType rightType = VT_VOID) {
-        out->addInsn(BC_IFICMPNE);
-        Cmp(out, leftType, rightType);
+        Cmp(out, BC_IFICMPNE, leftType, rightType);
     }
 
     void CmpGt(Bytecode* out, VarType leftType = VT_VOID, VarType rightType = VT_VOID) {
-        out->addInsn(BC_IFICMPG);
-        Cmp(out, leftType, rightType);
+        Cmp(out, BC_IFICMPG, leftType, rightType);
     }
 
     void CmpGe(Bytecode* out, VarType leftType = VT_VOID, VarType rightType = VT_VOID) {
-        out->addInsn(BC_IFICMPGE);
-        Cmp(out, leftType, rightType);
+        Cmp(out, BC_IFICMPGE, leftType, rightType);
     }
 
     void CmpLt(Bytecode* out, VarType leftType = VT_VOID, VarType rightType = VT_VOID) {
-        out->addInsn(BC_IFICMPL);
-        Cmp(out, leftType, rightType);
+        Cmp(out, BC_IFICMPL, leftType, rightType);
     }
 
     void CmpLe(Bytecode* out, VarType leftType = VT_VOID, VarType rightType = VT_VOID) {
-        out->addInsn(BC_IFICMPLE);
-        Cmp(out, leftType, rightType);
+        Cmp(out, BC_IFICMPLE, leftType, rightType);
     }
 
-    void Cmp(Bytecode* out, VarType leftType, VarType rightType) {
-        out->addUInt16(InsnSize[BC_ILOAD0] + InsnSize[BC_JA]);
+    void Not(Bytecode* out, VarType type = VT_VOID) {
+        out->addInsn(BC_ILOAD0);
+        CmpEq(out);
+    }
+
+private:
+
+    void Cmp(Bytecode* out, Instruction cmpInsn, VarType leftType, VarType rightType) {
+        out->addInsn(cmpInsn);
+        const uint16_t jump_offset_size = sizeof(uint16_t);
+        out->addUInt16(jump_offset_size + InsnSize[BC_ILOAD0] + InsnSize[BC_JA]);
         // :
         out->addInsn(BC_ILOAD0);    // push "0"
         out->addInsn(BC_JA);
@@ -80,12 +83,6 @@ public:
 //                out->addInsn(BC_POP);
 //                out->addInsn(BC_SWAP);
 //                out->addInsn(BC_POP);
-    }
-
-    void Not(Bytecode* out, VarType type = VT_VOID) {
-        out->addInsn(BC_ILOAD0);
-
-        CmpEq(out);
     }
 
     static const uint8_t InsnSize[];
