29787ebbcfa558a0fbbc93502b98fd25e9c4fc4b
doredox@gmail.com <doredox@gmail.com@c972e689-a6e6-bf8a-7419-c86a4d87c60c>
2012-11-20 19:06:29 +0000

    Simple interpreter and few bc generating bugs fixed

--- trunk/students/2012/korovin/bytecode/BytecodeImpl.h
+++ trunk/students/2012/korovin/bytecode/BytecodeImpl.h
@@ -18,7 +18,20 @@ public:
 	virtual ~BytecodeImpl();
 
 	virtual Status* execute(vector<Var*>& vars);
+
+	void executeFunction(BytecodeFunction* f);
 };
+
+template<typename T>
+uint64_t compare(T v1, T v2) {
+	if (v1 > v2)
+		return 1;
+	if (v1 == v2)
+		return 0;
+	if (v1 < v2)
+		return -1;
+	return 0;
+}
 
 } /* namespace mathvm */
 #endif /* BYTECODEIMPL_H_ */
