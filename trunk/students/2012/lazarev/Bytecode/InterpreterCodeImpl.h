73b5eda5eafa132a212d55ecec928cdfca701765
lazarev.sergey.89@gmail.com <lazarev.sergey.89@gmail.com@c972e689-a6e6-bf8a-7419-c86a4d87c60c>
2012-12-22 23:19:46 +0000

    memory leaks fixed

--- trunk/students/2012/lazarev/Bytecode/InterpreterCodeImpl.h
+++ trunk/students/2012/lazarev/Bytecode/InterpreterCodeImpl.h
@@ -6,16 +6,17 @@
 #include <cstring>
 #include "stdint.h"
 #include "mathvm.h"
-
+#include <tr1/memory>
 
 namespace mathvm {
 
 using std::string;
 using std::stack;
+using std::tr1::shared_ptr;
 
 
 class InterpreterCodeImpl: public Code {
-	stack<Var*> programStack;
+	stack< shared_ptr<Var> > programStack;
 	stack<Bytecode*> bytecodes;
 	stack<size_t> bcis;
 	void getBytecode();
@@ -23,16 +24,16 @@ class InterpreterCodeImpl: public Code {
 	double popDouble();
 	int64_t popInt();
 	const char *popString();
-	Var* pop();
+	shared_ptr<Var> pop();
 
-	void push(Var *val);
+	void push(shared_ptr<Var>);
 	void runBytecode(Bytecode *bytecode);
 	
-	Var* getDoubleVar(double val);
-	Var* getIntVar(int64_t val);
-	Var* getStringVar(const char* val);
+	shared_ptr<Var> getDoubleVar(double val);
+	shared_ptr<Var> getIntVar(int64_t val);
+	shared_ptr<Var> getStringVar(const char* val);
 	
-	Var* mem[1 << 16];
+	shared_ptr<Var> mem[1 << 16];
 	
 	
 
