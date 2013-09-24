0ab541ca36e62d3c5606038c7a5232bc0556e7a6
hatless.fox@gmail.com <hatless.fox@gmail.com@c972e689-a6e6-bf8a-7419-c86a4d87c60c>
2013-09-24 21:39:25 +0000

    Fix indentation in main

--- trunk/students/2013/huletski/0-AstPrinter/main.cpp
+++ trunk/students/2013/huletski/0-AstPrinter/main.cpp
@@ -16,15 +16,15 @@ const char * DEF_EXPR =
 "function void bar () {print(3, '\n');return;}function int foo () {	print(1, '\n');	return 2;}foo();bar();";
 
 int main(int argc, char** argv) {
-    const char * expr;
-    if (argc > 1) {
+	const char * expr;
+	if (argc > 1) {
 		char const * fileName = argv[1];
-        expr = loadFile(fileName);
-        if (expr == 0) {
-            printf("Cannot read file: %s\n", fileName);
-            return 1;
-        }
-    } else {
+		expr = loadFile(fileName);
+		if (expr == 0) {
+			printf("Cannot read file: %s\n", fileName);
+			return 1;
+		}
+	} else {
 		size_t defExprLen = strlen(DEF_EXPR) + 1;
 		expr = new char[defExprLen]();
 		//it's ok to rm const for init purposes
@@ -32,24 +32,24 @@ int main(int argc, char** argv) {
 	}
 
 	Parser parser;
-    Status* status = parser.parseProgram(expr);
-    if (status && status->isError()) {
-        uint32_t position = status->getPosition();
-        uint32_t line = 0, offset = 0;
-        positionToLineOffset(expr, position, line, offset);
-        printf("Cannot translate expression: expression at %d,%d; "
-               "error '%s'\n",
-               line, offset,
-               status->getError().c_str());
-    } else {
+	Status* status = parser.parseProgram(expr);
+	if (status && status->isError()) {
+		uint32_t position = status->getPosition();
+		uint32_t line = 0, offset = 0;
+		positionToLineOffset(expr, position, line, offset);
+		printf("Cannot translate expression: expression at %d,%d; "
+			   "error '%s'\n",
+			   line, offset,
+			   status->getError().c_str());
+	} else {
 		//print AST
 		AstPrinter printer;
 		printer.print(parser.top()->node());
-    }
+	}
 	
 	//clean up
-    delete status;
+	delete status;
 	delete [] expr;
 
-    return 0;
+	return 0;
 }
