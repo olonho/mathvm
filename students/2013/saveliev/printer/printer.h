1b18b5aab33568b5af464f643088a439a5e33bf6
vladislav.sav@gmail.com <vladislav.sav@gmail.com@c972e689-a6e6-bf8a-7419-c86a4d87c60c>
2013-09-30 16:50:19 +0000

    Updated printer

--- students/2013/saveliev/printer/printer.h
+++ students/2013/saveliev/printer/printer.h
@@ -9,30 +9,39 @@
 
 namespace mathvm {
 
-class Printer : public AstVisitor {
+class Printer: AstVisitor {
 public:
-    explicit Printer(std::ostream& out = std::cout): 
-        level(0), out(out), next_line_after_block(true) {
-    }
-
-    void print(AstFunction* root) {
-        if (root) {
-            printBlockContents(root->node()->body());
-        }
-    }
-
-#define VISITOR_FUNCTION_DECLARATION(type, name) \
-    virtual void visit##type(type* node);
-
-    FOR_NODES(VISITOR_FUNCTION_DECLARATION)
-#undef VISITOR_FUNCTION_DECLARATION
-
+    Printer(std::ostream& out = std::cout):
+        out(out), level(-1) {}
+    
+    void print(AstFunction* root) {        
+        assert(root);                
+        root->node()->body()->visit(this);
+    } 
+    
 private:
-    void printBlockContents(mathvm::BlockNode* node);
-
+    std::ostream& out;   
     int level;
-    std::ostream& out;
-    int next_line_after_block;
+      
+    virtual void visitBinaryOpNode(BinaryOpNode* node);
+    virtual void visitUnaryOpNode(UnaryOpNode* node);    
+    virtual void visitStringLiteralNode(StringLiteralNode* node);
+    virtual void visitDoubleLiteralNode(DoubleLiteralNode* node);
+    virtual void visitIntLiteralNode(IntLiteralNode* node);
+    virtual void visitLoadNode(LoadNode* node);
+    virtual void visitStoreNode(StoreNode* node);
+    virtual void visitForNode(ForNode* node);
+    virtual void visitWhileNode(WhileNode* node);
+    virtual void visitIfNode(IfNode* node);  
+    virtual void visitBlockNode(BlockNode* node);
+    void blockNodeVariableDeclarations(BlockNode* node);
+    void blockNodeFunctionDeclarations(BlockNode* node);
+    void blockNodeInnerNodes(BlockNode* node);
+    virtual void visitFunctionNode(FunctionNode* node);
+    virtual void visitReturnNode(ReturnNode* node);
+    virtual void visitCallNode(CallNode* node);
+    virtual void visitNativeCallNode(NativeCallNode* node);
+    virtual void visitPrintNode(PrintNode* node);
 
     void indent() {
         for (int i = 0; i < level; i++)
