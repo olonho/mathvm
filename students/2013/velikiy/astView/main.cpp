d03b3eac0ecc49bbad6562903664289de1987a7e
gmpota@gmail.com <gmpota@gmail.com@c972e689-a6e6-bf8a-7419-c86a4d87c60c>
2013-10-01 14:35:52 +0000

    done

---
+++ students/2013/velikiy/astView/main.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+
+#include "mathvm.h"
+#include "main.h"
+#include "printastvisitor.h"
+
+using namespace mathvm;
+using namespace std;
+
+int main(int argc, char** argv) {
+
+    if (argc < 2) {
+        cout << "Need *.mvm file" << endl;
+        return 3;
+    }
+
+    const char* programSourceFile = argv[1];
+    const char* expr = loadFile(programSourceFile);
+
+    if (expr == 0) {
+        cerr << "Cannot read file: " << programSourceFile << endl;
+        return 1;
+    }
+
+    Parser parser;
+    Status* status = parser.parseProgram(expr);
+    if (status != NULL && status->isError()) {
+        uint32_t position = status->getPosition();
+        uint32_t line = 0, offset = 0;
+        positionToLineOffset(expr, position, line, offset);
+        cerr << "Cannot translate expression: expression at "
+                << line << ", " << offset << "; " << endl
+                << "error "
+                << status->getError().c_str() << endl;
+        delete[] expr;
+        return 2;
+    }
+
+    showFormatedCode(parser.top());
+
+    delete[] expr;
+
+    return 0;
+
+}
+
+void showFormatedCode(const AstFunction* rootFunction) {
+
+    PrintAstVisitor printVisitor;
+
+    printVisitor.visitTopFunction(rootFunction);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
