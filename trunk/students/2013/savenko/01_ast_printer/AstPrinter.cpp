7ade6c1d0b5212c1c6284a3f680dce14b75b3a2d
deadok22 <deadok22@c972e689-a6e6-bf8a-7419-c86a4d87c60c>
2013-10-02 17:51:16 +0000

    added initial version of ast printer

--- trunk/students/2013/savenko/01_ast_printer/AstPrinter.cpp
+++ trunk/students/2013/savenko/01_ast_printer/AstPrinter.cpp
@@ -3,110 +3,154 @@
 
 namespace mathvm {
 
-namespace _ast_printer_utils {
-  
-  const std::string VT_INVALID_STR("invalid");
-  const std::string VT_VOID_STR("void");
-  const std::string VT_DOUBLE_STR("double");
-  const std::string VT_INT_STR("int");
-  const std::string VT_STRING_STR("string");
-  
-  std::string const & getVarTypeString(VarType type) {
-    switch(type) {
-      case VT_VOID    : return VT_VOID_STR;
-      case VT_DOUBLE  : return VT_DOUBLE_STR;
-      case VT_INT     : return VT_INT_STR;
-      case VT_STRING  : return VT_STRING_STR;
-      default         : return VT_INVALID_STR;
-    }
-  }
-    
-  void printFunctionParameters(FunctionNode * functionNode) {
-    for (uint32_t i = 0; i != functionNode->parametersNumber(); ++i) {
-      std::cout << getVarTypeString(functionNode->parameterType(i))
-                << " "
-                << functionNode->parameterName(i);
-      if (i != functionNode->parametersNumber()) {
-        std::cout << ", ";
+// utility
+
+std::string unescape(std::string const & str) {
+  std::string result;
+  for (std::string::size_type i = 0; i != str.size(); ++i) {
+    char ch = str[i];
+    switch (ch) {
+      case '\n': {
+        result += "\\n";
+        break;
+      }
+      case '\t': {
+        result += "\\t";
+        break;
+      }
+      case '\r': {
+        result += "\\r";
+        break;
+      }
+      case '\\': {
+        result += "\\\\";
+        break;
+      }
+      default: {
+        result += ch;
       }
     }
   }
-
+  return result;
 }
 
 // visitor implementation
 
-using namespace _ast_printer_utils;
-
 void AstPrinter::visitBinaryOpNode(BinaryOpNode * binaryOpNode) {
-  std::cout << "binop" << std::endl;
+  binaryOpNode->left()->visit(this);
+  std::cout << " " << tokenOp(binaryOpNode->kind()) << " ";
+  binaryOpNode->right()->visit(this);
 }
 
 void AstPrinter::visitUnaryOpNode(UnaryOpNode * unaryOpNode) {
-  std::cout << "uop" << std::endl;
+  std::cout << tokenOp(unaryOpNode->kind());
+  unaryOpNode->operand()->visit(this);
 }
 
 void AstPrinter::visitStringLiteralNode(StringLiteralNode * stringLiteralNode) {
-  std::cout << "string literal" << std::endl;
+  std::cout << '\'' << unescape(stringLiteralNode->literal()) << '\'';
 }
 
 void AstPrinter::visitDoubleLiteralNode(DoubleLiteralNode * doubleLiteralNode) {
-  std::cout << "double literal" << std::endl;
+  std::cout << doubleLiteralNode->literal();
 }
 
-void AstPrinter::visitIntLiteralNode(IntLiteralNode * IntLiteralNode) {
-  std::cout << "IntLiteral" << std::endl;
+void AstPrinter::visitIntLiteralNode(IntLiteralNode * intLiteralNode) {
+  std::cout << intLiteralNode->literal();
 }
 
-void AstPrinter::visitLoadNode(LoadNode * LoadNode) {
-  std::cout << "Load" << std::endl;
+void AstPrinter::visitLoadNode(LoadNode * loadNode) {
+  std::cout << loadNode->var()->name();
 }
 
-void AstPrinter::visitStoreNode(StoreNode * StoreNode) {
-  std::cout << "Store" << std::endl;
+void AstPrinter::visitStoreNode(StoreNode * storeNode) {
+  std::cout << storeNode->var()->name()
+            << " " << tokenOp(storeNode->op()) << " ";
+  storeNode->value()->visit(this);
 }
 
-void AstPrinter::visitForNode(ForNode * ForNode) {
-  std::cout << "For" << std::endl;
+void AstPrinter::visitForNode(ForNode * forNode) {
+  std::cout << "for (" << forNode->var()->name() << " in ";
+  forNode->inExpr()->visit(this);
+  std::cout << ")" << std::endl;
+  forNode->body()->visit(this);
 }
 
-void AstPrinter::visitWhileNode(WhileNode * WhileNode) {
-  std::cout << "While" << std::endl;
+void AstPrinter::visitWhileNode(WhileNode * whileNode) {
+  std::cout << "while (";
+  whileNode->whileExpr()->visit(this);
+  std::cout << ")" << std::endl;
+  whileNode->loopBlock()->visit(this);
 }
 
-void AstPrinter::visitIfNode(IfNode * IfNode) {
-  std::cout << "If" << std::endl;
+void AstPrinter::visitIfNode(IfNode * ifNode) {
+  std::cout << "if (";
+  ifNode->ifExpr()->visit(this);
+  std::cout << ")" << std::endl;
+  ifNode->thenBlock()->visit(this);
+  if (ifNode->elseBlock()) {
+    std::cout << indent() << "else" << std::endl;
+    ifNode->elseBlock()->visit(this);
+  }
 }
 
-void AstPrinter::visitBlockNode(BlockNode * BlockNode) {
-  std::cout << "Block" << std::endl;
+void AstPrinter::visitBlockNode(BlockNode * blockNode) {
+  std::cout << indent() << "{" << std::endl;
+  increaseIndent();
+  for (uint32_t i = 0; i < blockNode->nodes(); ++i) {
+    std::cout << indent();
+    blockNode->nodeAt(i)->visit(this);
+    std::cout << ";" << std::endl;
+  }
+  decreaseIndent();
+  std::cout << indent() << "}" << std::endl;
 }
 
 void AstPrinter::visitFunctionNode(FunctionNode * functionNode) {
-  std::cout << "function "
-            << getVarTypeString(functionNode->returnType())
-            << " " 
-            << functionNode->name()
-            << "(";
-  printFunctionParameters(functionNode);         
-  std::cout << ")"
-            << std::endl;
+  std::cout << indent() << "function "
+            << typeToName(functionNode->returnType())
+            << " " << functionNode->name() << "(";
+  for (uint32_t i = 0; i != functionNode->parametersNumber(); ++i) {
+    std::cout << typeToName(functionNode->parameterType(i))
+              << " "
+              << functionNode->parameterName(i);
+    if (i != functionNode->parametersNumber() - 1) {
+      std::cout << ", ";
+    }
+  }
+  std::cout << ")" << std::endl;
+  visitBlockNode(functionNode->body());
 }
 
-void AstPrinter::visitReturnNode(ReturnNode * ReturnNode) {
-  std::cout << "Return" << std::endl;
+void AstPrinter::visitReturnNode(ReturnNode * returnNode) {
+  std::cout << indent() << "return ";
+  returnNode->returnExpr()->visit(this);
 }
 
-void AstPrinter::visitCallNode(CallNode * CallNode) {
-  std::cout << "Call" << std::endl;
+void AstPrinter::visitCallNode(CallNode * callNode) {
+  std::cout << callNode->name() << "(";
+  for (uint32_t i = 0; i != callNode->parametersNumber(); ++i) {
+    callNode->parameterAt(i)->visit(this);
+    if (i != callNode->parametersNumber() - 1) {
+      std::cout << ", ";
+    }
+  }
+  std::cout << ")";
 }
 
 void AstPrinter::visitNativeCallNode(NativeCallNode * NativeCallNode) {
   std::cout << "NativeCall" << std::endl;
 }
 
-void AstPrinter::visitPrintNode(PrintNode * PrintNode) {
-  std::cout << "Print" << std::endl;
+void AstPrinter::visitPrintNode(PrintNode * printNode) {
+  std::cout << "print (";
+  for (uint32_t i = 0; i != printNode->operands(); ++i) {
+    printNode->operandAt(i)->visit(this);
+    if (i != printNode->operands() - 1) {
+      std::cout << ", ";
+    }
+  }
+  std::cout << ")";
 }
 
 
