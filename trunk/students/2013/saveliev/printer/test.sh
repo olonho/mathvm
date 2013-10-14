a0593145ddc74982180a57926c7662ebc292caba
vladislav.sav@gmail.com <vladislav.sav@gmail.com@c972e689-a6e6-bf8a-7419-c86a4d87c60c>
2013-10-14 22:33:42 +0000

    Translator and interpreter

--- trunk/students/2013/saveliev/printer/test.sh
+++ trunk/students/2013/saveliev/printer/test.sh
@@ -1,3 +1,4 @@
+make
 python ../../../../tests/astprint/run.py -e build/debug/printer -d ../../../../tests
 python ../../../../tests/astprint/run.py -e build/debug/printer -d ../../../../tests/optional
 python ../../../../tests/astprint/run.py -e build/debug/printer -d ../../../../tests/additional
