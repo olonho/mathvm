136f6d31b7ccceb7b783bc77eaea734c78c849ce
mapseamoff@mail.ru <mapseamoff@mail.ru@c972e689-a6e6-bf8a-7419-c86a4d87c60c>
2013-10-20 09:10:24 +0000

    bytecode translator/interpreter initial

---
+++ students/2013/kartashov/interpreter/bcinterpreter.h
@@ -0,0 +1,138 @@
+#ifndef BCINTERPRETER_H
+#define BCINTERPRETER_H
+
+#include "mathvm.h"
+#include "ast.h"
+#include <stack>
+
+namespace mathvm {
+
+struct InterpreterCodeImpl : public Code {
+public:
+    InterpreterCodeImpl() : Code() {}
+    Status *execute(vector<Var *> &vars);
+
+private:
+    Bytecode *currentBC() {
+        return currentScope->function->bytecode();
+    }
+
+    uint32_t &currentIP() {
+        return currentScope->ip;
+    }
+
+#define GET_FROM_BC(type) \
+    type v = currentBC()->getTyped<type>(currentIP()); \
+    currentIP() += sizeof(type); \
+    return v;
+
+    uint16_t getUInt16() { GET_FROM_BC(uint16_t) }
+    double getDouble() { GET_FROM_BC(double) }
+    int16_t getInt16() { GET_FROM_BC(int16_t) }
+    int64_t getInt64() { GET_FROM_BC(int64_t) }
+#undef GET_FROM_BC
+
+    void doubleBinOp(TokenKind op);
+    void intBinOp(TokenKind op);
+
+    inline void loadVar(uint16_t index);
+    inline void loadVar(uint16_t cid, uint16_t index);
+    inline void storeVar(uint32_t index);
+    inline void storeVar(uint16_t cid, uint16_t index);
+
+    void callFunction(uint16_t id) {
+        BytecodeFunction *f = (BytecodeFunction*)functionById(id);
+        currentScope = new IScope(f, currentScope);
+    }
+
+    void callNative(uint16_t id);
+
+    //------------------------------------------------------------
+
+    class Any {
+    public:
+        Any() : content(0) {}
+        Any(const Any &other) : content (other.content ? other.content->clone() : 0) {}
+
+        template<typename T>
+        Any(const T &val) : content(new Holder<T>(val)) {}
+
+        ~Any() {
+            delete content;
+        }
+
+        Any &operator=(const Any &other) {
+            Any(other).swap(*this);
+            return *this;
+        }
+
+        template<typename T>
+        T get() {
+            return static_cast<Any::Holder<T> *>(content)->val;
+        }
+
+        double getDouble()      { return get<double>(); }
+        int64_t getInt()        { return get<int64_t>(); }
+        uint16_t getUInt16()    { return get<uint16_t>(); }
+        std::string getString() { return std::string(get<char *>()); }
+
+        template<typename T>
+        T *getPtr() {
+            return &static_cast<Any::Holder<T> *>(content)->val;
+        }
+
+        double *getDoublePtr()      { return getPtr<double>(); }
+        int64_t *getIntPtr()        { return getPtr<int64_t>(); }
+        uint16_t *getUInt16Ptr()    { return getPtr<uint16_t>(); }
+        char *getStringPtr()        { return *getPtr<char *>(); }
+
+        void swap(Any &other) {
+            std::swap(content, other.content);
+        }
+
+    private:
+        class AnyContainer {
+        public:
+            virtual ~AnyContainer() { }
+            virtual AnyContainer *clone() const = 0;
+        };
+
+        template<class T>
+        struct Holder : public AnyContainer {
+            Holder(T v) : AnyContainer(), val(v) {}
+            T val;
+
+            AnyContainer *clone() const {
+                return new Holder(val);
+            }
+        };
+
+        AnyContainer *content;
+    };
+
+    class IScope {
+    public:
+        IScope(BytecodeFunction *f, IScope *p = 0) : ip(0), parent(p), function(f), vars(f->localsNumber() + f->parametersNumber()) {
+        }
+
+        uint32_t ip;
+        IScope *parent;
+        BytecodeFunction *function;
+        std::vector<Any> vars;
+    };
+
+    //------------------------------------------------------------
+
+    Any popStack() {
+        Any res = pstack.top();
+        pstack.pop();
+        return res;
+    }
+
+    IScope *currentScope;
+    std::stack<Any> pstack;
+};
+
+}
+
+#endif // BCINTERPRETER_H
---
+++ students/2013/kartashov/interpreter/bcprinter.cpp
@@ -0,0 +1,34 @@
+#include "bctranslator.h"
+#include "bcinterpreter.h"
+
+using namespace mathvm;
+
+int main(int argc, char *argv[]) {
+    if(argc != 2) {
+        std::cout << "Usage: " << argv[0] << " <program.mvm>" << std::endl;
+        return -1;
+    }
+
+    char *inCode = loadFile(argv[1]);
+    if(!inCode) {
+        std::cout << "Unable to open file: " << argv[1] << std::endl;
+        return -1;
+    }
+
+    BCTranslator t;
+    InterpreterCodeImpl *outCode = 0;
+    Status *res = t.translate(inCode, (Code**)&outCode);
+    if(res && res->isError()) {
+        std::cout << "BytecodeTranslator error: "  << res->getError();
+        if(res->getPosition() != (uint32_t)-1) {
+            uint32_t line, offset;
+            positionToLineOffset(inCode, res->getPosition(), line, offset);
+            std::cout << " at line " << line << " pos " << offset;
+        }
+        std::cout << std::endl;
+        return -1;
+    }
+
+    outCode->disassemble();
+    return 0;
+}
