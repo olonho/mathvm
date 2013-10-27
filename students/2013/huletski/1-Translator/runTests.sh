138a4d9e1129b0af8605b37458da57f2d6e66d02
hatless.fox@gmail.com <hatless.fox@gmail.com@c972e689-a6e6-bf8a-7419-c86a4d87c60c>
2013-10-27 19:57:15 +0000

    [Lab1] Initial translator implemantation + interpreter for testing

---
+++ students/2013/huletski/1-Translator/runTests.sh
@@ -0,0 +1,6 @@
+TEST_SCRIPT="./../../../../tests/run.py"
+TESTEE="./build/debug/mvm"
+
+$TEST_SCRIPT -e $TESTEE -t ./../../../../tests/
+#$TEST_SCRIPT -e $TESTEE -d ./../../../../tests/optional
+#$TEST_SCRIPT -e $TESTEE -d ./../../../../tests/additional
