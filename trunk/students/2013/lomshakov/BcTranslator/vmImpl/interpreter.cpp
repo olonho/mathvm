4aba675f7f8ce3e0c66f859708dc89226658806d
vadim.lomshakov@gmail.com <vadim.lomshakov@gmail.com@c972e689-a6e6-bf8a-7419-c86a4d87c60c>
2013-10-30 20:48:41 +0000

    fix SVN 405 Method Not Allowed

---
+++ trunk/students/2013/lomshakov/BcTranslator/vmImpl/interpreter.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Vadim Lomshakov on 10/26/13.
+// Copyright (c) 2013 spbau. All rights reserved.
+//
+
+
+#include "interpreter.h"
+
+namespace mathvm {
+
+
+  Status *InterpreterCodeImpl::execute(vector<Var *> &vars) {
+
+//    switch ()
+
+    return 0;
+  }
+}
