78069495fb5fffb834e6d6c1645609aeff5bd11e
hatless.fox@gmail.com <hatless.fox@gmail.com@c972e689-a6e6-bf8a-7419-c86a4d87c60c>
2013-12-17 09:23:38 +0000

    [Lab 2] Initial Compiler (JIT) implementation

---
+++ trunk/students/2013/huletski/3-Compiler/LokiMachCodeTranslator.h
@@ -0,0 +1,44 @@
+//
+//  LokiMachCodeTranslator.h
+//  VM_3
+//
+//  Created by Hatless Fox on 12/11/13.
+//  Copyright (c) 2013 WooHoo. All rights reserved.
+//
+
+#ifndef VM_3_LokiMachCodeTranslator_h
+#define VM_3_LokiMachCodeTranslator_h
+
+#include <iostream>
+#include "MachCodeContainer.h"
+#include "mathvm.h"
+#include "parser.h"
+
+#include "AstToMCConverter.h"
+#include "AstInfoExtractor.h"
+
+using namespace mathvm;
+
+class LokiMachCodeTranslator : public Translator {
+public: // methods
+  virtual Status* translate(const string& program, Code ** code) {
+    Status* status = m_parser.parseProgram(program);
+    if (status && status->isError()) { return status; }
+    
+    AstFunction *top_function = m_parser.top();
+    AstToMCConverter converter;
+    
+    AstInfoExtractor extractor(&converter);
+    extractor.handle_function_definition(top_function);
+    //std::cout << "------------------------------" << std::endl;
+    converter.convert(extractor.functions(), extractor.functions().size());
+     
+    *code = converter.code();
+    return converter.errorStatus();
+  }
+
+private: //members
+  Parser m_parser;
+};
+
+#endif
