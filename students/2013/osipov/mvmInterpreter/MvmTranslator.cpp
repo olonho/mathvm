4583d940c8861db79cb34a42a76fcc8b2b0018d2
stasstels@gmail.com <stasstels@gmail.com@c972e689-a6e6-bf8a-7419-c86a4d87c60c>
2014-01-08 22:46:21 +0000

    Small fix

--- students/2013/osipov/mvmInterpreter/MvmTranslator.cpp
+++ students/2013/osipov/mvmInterpreter/MvmTranslator.cpp
@@ -15,15 +15,5 @@ Status* MvmTranslator::translate(const string& program, Code**code) {
 
     MvmBytecode* bcode = new MvmBytecode();
     *code = bcode;
-//    varmap_t varmap;
-//    variable_t context = introduce_var(parser.top()->node()->body(), varmap, 0,
-//            "@context", VT_INT);
-//    funmap_t funmap;
-//    try {
-//        BytecodeBlockTranslator().run(bcode, bcode->bytecode(), varmap, funmap,
-//                VT_VOID, context.id, parser.top()->node()->body());
-//    } catch (translator_exception e) {
-//        return new Status(e.what(), e.position());
-//    }
     return 0;
 }
