95e136e8ced8be2ecf3c2dd97703413a919a67b9
ivanovak742@gmail.com <ivanovak742@gmail.com@c972e689-a6e6-bf8a-7419-c86a4d87c60c>
2014-10-19 15:27:03 +0000

    Implementations of visit for the rest of the nodes

--- students/2014/ivanov/AstPrinter/ast_printer.cpp
+++ students/2014/ivanov/AstPrinter/ast_printer.cpp
@@ -8,63 +8,111 @@ namespace mathvm {
 
 struct AstToStringVisitor : public AstVisitor {
   virtual void visitUnaryOpNode(UnaryOpNode* node) override {
-    node->visitChildren(this);
+    ss << tokenOp(node->kind()) << '(';
+    node->operand()->visit(this);
+    ss << ')';
   }
+
   virtual void visitBinaryOpNode(BinaryOpNode* node) override {
-    node->visitChildren(this);
+    ss << '(';
+    node->left()->visit(this);
+    ss << ") " << tokenOp(node->kind()) << " (";
+    node->right()->visit(this);
+    ss << ')';
   }
+
   virtual void visitStringLiteralNode(StringLiteralNode* node) override {
-    node->visitChildren(this);
+    ss << '\'' << node->literal() << '\'';
   }
+
   virtual void visitDoubleLiteralNode(DoubleLiteralNode* node) override {
-    node->visitChildren(this);
+    ss << node->literal();
   }
+
   virtual void visitIntLiteralNode(IntLiteralNode* node) override {
-    node->visitChildren(this);
+    ss << node->literal();
   }
+
   virtual void visitLoadNode(LoadNode* node) override {
-    node->visitChildren(this);
+    ss << node->var()->name();
   }
+
   virtual void visitStoreNode(StoreNode* node) override {
-    node->visitChildren(this);
+    ss << node->var()->name() << ' ' << tokenOp(node->op()) << ' ';
+    node->value()->visit(this);
+    ss << ';' << endl;
   }
+
   virtual void visitForNode(ForNode* node) override {
-    node->visitChildren(this);
+    ss << "for (" << node->var()->name() << " in ";
+    node->inExpr()->visit(this);
+    ss << ") ";
+    node->body()->visit(this);
   }
+
   virtual void visitWhileNode(WhileNode* node) override {
-    node->visitChildren(this);
+    ss << "while (";
+    node->whileExpr()->visit(this);
+    ss << ") ";
+    node->loopBlock()->visit(this);
   }
+
   virtual void visitIfNode(IfNode* node) override {
-    node->visitChildren(this);
+    ss << "if (";
+    node->ifExpr()->visit(this);
+    ss << ") ";
+    node->thenBlock()->visit(this);
+    ss << " else ";
+    node->elseBlock()->visit(this);
   }
+
   virtual void visitBlockNode(BlockNode* node) override {
+    ss << "{" << endl;
+    Scope::VarIterator vi = Scope::VarIterator(node->scope());
+    while (vi.hasNext()) {
+      AstVar *v = vi.next();
+      ss << typeToName(v->type()) << ' ' << v->name() << ';' << endl;
+    }
     node->visitChildren(this);
+    ss << "}" << endl;
   }
 
   virtual void visitFunctionNode(FunctionNode* node) override {
     ss << typeToName(node->returnType()) << " (";
-    for (uint32_t i = 0;; i++) {
-      if (i != node->parametersNumber()) {
-        ss << typeToName(node->parameterType(i)) << ' ' << node->parameterName(i);
-        ss << ", ";
-      } else break;
+    for (uint32_t i = 0; i < node->parametersNumber(); i++) {
+      ss << typeToName(node->parameterType(i)) << ' ' << node->parameterName(i);
+      if (i < node->parametersNumber() - 1) ss << ", ";
     }
-    ss << ") {";
-    visitBlockNode(node->body());
-    ss << "\n}";
+    ss << ") ";
+    node->body()->visit(this);
   }
 
   virtual void visitReturnNode(ReturnNode* node) override {
-    node->visitChildren(this);
+    ss << "return ";
+    node->returnExpr()->visit(this);
+    ss << ";" << endl;
   }
+
   virtual void visitCallNode(CallNode* node) override {
-    node->visitChildren(this);
+    ss << node->name() << " (";
+    for (uint32_t i = 0; i < node->parametersNumber(); i++) {
+      node->parameterAt(i)->visit(this);
+      if (i < node->parametersNumber() - 1) ss << ", ";
+    }
+    ss << ");" << endl;
   }
+
   virtual void visitNativeCallNode(NativeCallNode* node) override {
-    node->visitChildren(this);
+    // TODO
   }
+
   virtual void visitPrintNode(PrintNode* node) override {
-    node->visitChildren(this);
+    ss << "print(";
+    for (uint32_t i = 0; i < node->operands(); i++) {
+      node->operandAt(i)->visit(this);
+      if (i < node->operands() - 1) ss << ", ";
+    }
+    ss << ");" << endl;
   }
 
   stringstream ss;
@@ -76,7 +124,7 @@ struct AstPrinter : public Translator {
     Status *status = parser.parseProgram(program);
     if (status != 0 && status->isError()) return status;
     AstToStringVisitor stringVisitor;
-    parser.top()->node()->visit(&stringVisitor);
+    parser.top()->node()->body()->visit(&stringVisitor);
     cout << stringVisitor.ss.str() << endl;
 
     return new Status("No executable code produced");
