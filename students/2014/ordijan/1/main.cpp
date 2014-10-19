4618e2a906dcded449e76a702e2834dab0e67f95
andronik.ordian@gmail.com <andronik.ordian@gmail.com@c972e689-a6e6-bf8a-7419-c86a4d87c60c>
2014-10-19 15:38:59 +0000

    AstPrinter

---
+++ students/2014/ordijan/1/main.cpp
@@ -0,0 +1,64 @@
+#include "mathvm.h"
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+#include <iostream>
+
+using namespace mathvm;
+using namespace std;
+
+int main(int argc, char** argv) {
+    string impl = "printer";
+    const char* script = NULL;
+    for (int32_t i = 1; i < argc; i++) {
+      if (string(argv[i]) == "-j") {
+        impl = "jit";
+      } else {
+        script = argv[i];
+      }
+    }
+    Translator* translator = Translator::create(impl);
+
+    if (translator == 0) {
+        cout << "TODO: Implement translator factory in translator.cpp!!!!" << endl;
+        return 1;
+    }
+
+    const char* expr = "double x; double y;"
+                        "x += 8.0; y = 2.0;"
+                        "print('Hello, x=',x,' y=',y,'\n');"
+        ;
+    bool isDefaultExpr = true;
+
+    if (script != NULL) {
+        expr = loadFile(script);
+        if (expr == 0) {
+            printf("Cannot read file: %s\n", script);
+            return 1;
+        }
+        isDefaultExpr = false;
+    }
+
+    Code* code = 0;
+
+    Status* translateStatus = translator->translate(expr, &code);
+    if (translateStatus->isError()) {
+        uint32_t position = translateStatus->getPosition();
+        uint32_t line = 0, offset = 0;
+        positionToLineOffset(expr, position, line, offset);
+        printf("Cannot translate expression: expression at %d,%d; "
+               "error '%s'\n",
+               line, offset,
+               translateStatus->getError().c_str());
+    } 
+    delete translateStatus;
+    delete translator;
+
+    if (!isDefaultExpr) {
+      delete [] expr;
+    }
+
+    return 0;
+}
