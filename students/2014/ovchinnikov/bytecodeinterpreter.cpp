ff422e2937d2131a0a8eb8261399461a9333a0b8
ovchinnikov.daniel@gmail.com <ovchinnikov.daniel@gmail.com@c972e689-a6e6-bf8a-7419-c86a4d87c60c>
2014-12-06 14:25:13 +0000

    refactoring

--- students/2014/ovchinnikov/bytecodeinterpreter.cpp
+++ students/2014/ovchinnikov/bytecodeinterpreter.cpp
@@ -4,28 +4,18 @@
 #include <functional>
 #include <cmath>
 
-using std::cout;
-using std::showpoint;
-using std::endl;
-
 template <typename T> int64_t compare(T left, T right) { return left > right ? 1 : left == right ? 0 : -1; }
 
 Status *BytecodeInterpreter::execute(vector<Var *> &) try {
     #ifdef BCPRINT
-    Code::FunctionIterator it(this);
-    while (it.hasNext()) {
-        auto f = (BytecodeFunction *)it.next();
-        std::cout << f->id() << ": " << f->name() << std::endl;
-        f->bytecode()->dump(std::cout);
-        std::cout << std::endl;
-    }
+    disassemble();
     return Status::Ok();
     #endif
     processCall(0);
-    while (pointer < bc->length()) {
-        Instruction instruction = (Instruction)bc->get(pointer++);
+    while (pointer() < bc()->length()) {
+        Instruction instruction = next();
         switch (instruction) {
-            case BC_INVALID: throw "Invalid code";
+            case BC_INVALID:    throw "Invalid code";
             case BC_DLOAD:      operandStack.push_back(bc_read<double>()); break;
             case BC_ILOAD:      operandStack.push_back(bc_read<int64_t>()); break;
             case BC_SLOAD:      operandStack.push_back(bc_read<uint16_t>()); break;
@@ -50,54 +40,39 @@ Status *BytecodeInterpreter::execute(vector<Var *> &) try {
             case BC_IAXOR:      operandStack.push_back(pop_return().i ^ pop_return().i); break;
             case BC_DNEG:       operandStack.back().d = -operandStack.back().d; break;
             case BC_INEG:       operandStack.back().i = -operandStack.back().i; break;
-            case BC_DPRINT:     cout << pop_return().d; break;
-            case BC_IPRINT:     cout << pop_return().i; break;
-            case BC_SPRINT:     cout << constantById(pop_return().ui16); break;
+            case BC_DPRINT:     std::cout << pop_return().d; break;
+            case BC_IPRINT:     std::cout << pop_return().i; break;
+            case BC_SPRINT:     std::cout << constantById(pop_return().ui16); break;
             case BC_I2D:        operandStack.back().d = operandStack.back().i; break;
             case BC_D2I :       operandStack.back().i = operandStack.back().d; break;
-            case BC_SWAP: {
-                    Value upper = pop_return();
-                    Value lower = pop_return();
-                    operandStack.push_back(upper);
-                    operandStack.push_back(lower);
-                }; break;
+            case BC_SWAP:       std::iter_swap(operandStack.end() - 1, operandStack.end() - 2); break;
             case BC_POP:        operandStack.pop_back(); break;
             case BC_LOADDVAR:
             case BC_LOADIVAR:
-            case BC_LOADSVAR:   operandStack.push_back(variableStack[frame->v_offset + bc_read<uint16_t>()]); break;
+            case BC_LOADSVAR:   operandStack.push_back(value(bc_read<uint16_t>())); break;
             case BC_STOREDVAR:
             case BC_STOREIVAR:
-            case BC_STORESVAR:  variableStack[frame->v_offset + bc_read<uint16_t>()] = pop_return(); break;
+            case BC_STORESVAR:  value(bc_read<uint16_t>()) = pop_return(); break;
             case BC_LOADCTXDVAR:
             case BC_LOADCTXIVAR:
-            case BC_LOADCTXSVAR:
-                operandStack.push_back(
-                    variableStack[
-                        ctxOffset[bc_read<uint16_t>()].back() + bc_read<uint16_t>()
-                    ]
-                );
-                break;
+            case BC_LOADCTXSVAR: operandStack.push_back(value(bc_read<uint16_t>(), bc_read<uint16_t>())); break;
             case BC_STORECTXDVAR:
             case BC_STORECTXIVAR:
-            case BC_STORECTXSVAR:
-                variableStack[
-                    ctxOffset[bc_read<uint16_t>()].back() + bc_read<uint16_t>()
-                ] = pop_return();
-                break;
+            case BC_STORECTXSVAR: value(bc_read<uint16_t>(), bc_read<uint16_t>()) = pop_return(); break;
             case BC_DCMP:       operandStack.push_back(compare<double>(pop_return().d, pop_return().d)); break;
             case BC_ICMP:       operandStack.push_back(compare<int64_t>(pop_return().i, pop_return().i)); break;
-            case BC_JA:         pointer += bc->getInt16(pointer); break;
-            case BC_IFICMPE:    pointer += pop_return().i == pop_return().i ? bc->getInt16(pointer) : sizeof(int16_t); break;
-            case BC_IFICMPNE:   pointer += pop_return().i != pop_return().i ? bc->getInt16(pointer) : sizeof(int16_t); break;
-            case BC_IFICMPG:    pointer += pop_return().i >  pop_return().i ? bc->getInt16(pointer) : sizeof(int16_t); break;
-            case BC_IFICMPGE:   pointer += pop_return().i >= pop_return().i ? bc->getInt16(pointer) : sizeof(int16_t); break;
-            case BC_IFICMPL:    pointer += pop_return().i <  pop_return().i ? bc->getInt16(pointer) : sizeof(int16_t); break;
-            case BC_IFICMPLE:   pointer += pop_return().i <= pop_return().i ? bc->getInt16(pointer) : sizeof(int16_t); break;
+            case BC_JA:         pointer() += bc()->getInt16(pointer()); break;
+            case BC_IFICMPE:    pointer() += pop_return().i == pop_return().i ? bc()->getInt16(pointer()) : sizeof(int16_t); break;
+            case BC_IFICMPNE:   pointer() += pop_return().i != pop_return().i ? bc()->getInt16(pointer()) : sizeof(int16_t); break;
+            case BC_IFICMPG:    pointer() += pop_return().i >  pop_return().i ? bc()->getInt16(pointer()) : sizeof(int16_t); break;
+            case BC_IFICMPGE:   pointer() += pop_return().i >= pop_return().i ? bc()->getInt16(pointer()) : sizeof(int16_t); break;
+            case BC_IFICMPL:    pointer() += pop_return().i <  pop_return().i ? bc()->getInt16(pointer()) : sizeof(int16_t); break;
+            case BC_IFICMPLE:   pointer() += pop_return().i <= pop_return().i ? bc()->getInt16(pointer()) : sizeof(int16_t); break;
             case BC_CALL:       processCall(bc_read<uint16_t>()); break;
-            case BC_DUMP:       cout << operandStack.back().i << endl; break;
-            case BC_STOP:       return Status::Warning("Execution stopped");
             case BC_RETURN:     processReturn(); break;
             case BC_BREAK:      break;
+            case BC_DUMP:       std::cout << operandStack.back().i << std::endl; break;
+            case BC_STOP:       return Status::Warning("Execution stopped");
             default: throw "Not implemented"; break;
         }
     }
@@ -107,45 +82,53 @@ Status *BytecodeInterpreter::execute(vector<Var *> &) try {
 }
 
 void BytecodeInterpreter::processCall(uint16_t functionId) {
-    auto fun =  (BytecodeFunction *)functionById(functionId);
-    frame = new StackFrame {
-        frame,
+    auto fun = (BytecodeFunction *)functionById(functionId);
+    StackFrame frame = {
+        fun,
+        0,
         (uint32_t)variableStack.size(),
-        (uint32_t)operandStack.size() - fun->parametersNumber(),
-        pointer,
-        fun
+        (uint32_t)operandStack.size() - fun->parametersNumber()
     };
+    callStack.push_back(frame);
+    ctxOffset[functionId].push_back(frame.v_offset);
 
-    bc = frame->function->bytecode();
-    pointer = 0;
-    ctxOffset[functionId].push_back(frame->v_offset);
-
-    // move arguments to call stack
-    auto it = std::prev(operandStack.end(), frame->function->parametersNumber());
+    // move arguments to variable stack
+    auto it = std::prev(operandStack.end(), frame.function->parametersNumber());
     std::move(it, operandStack.end(), std::back_inserter(variableStack));
     operandStack.erase(it, operandStack.end());
-    variableStack.resize(variableStack.size() + frame->function->localsNumber());
+
+    #ifdef CALLPRINT
+    if (functionId != 0) {
+        std::cout << fun->name() << '(';
+        copy(
+            variableStack.begin() + frame.v_offset,
+            variableStack.end() - 1,
+            ostream_iterator<Value>(std::cout, ", ")
+        );
+        std::cout << variableStack.back();
+        std::cout << ')' << std::endl;
+    }
+    #endif
+
+    // allocate space for locals
+    variableStack.resize(variableStack.size() + frame.function->localsNumber());
 }
 
 inline void BytecodeInterpreter::processReturn() {
-    unique_ptr<StackFrame> leaving(frame);
-    // remove locals from stack
-    variableStack.resize(leaving->v_offset);
+    StackFrame frame = callStack.back();
+    callStack.pop_back();
+    ctxOffset[frame.function->id()].pop_back();
 
+    // remove locals from stack
+    variableStack.resize(frame.v_offset);
     // reset operand stack to previous state
     // (except for non-void function, in this case last operand, i.e. returned value is pushed onto stack back)
-    VarType returnType = leaving->function->returnType();
+    VarType returnType = frame.function->returnType();
     if (returnType == VT_VOID) {
-        operandStack.resize(leaving->o_offset);
+        operandStack.resize(frame.o_offset);
     } else {
         Value returnValue = operandStack.back();
-        operandStack.resize(leaving->o_offset);
+        operandStack.resize(frame.o_offset);
         operandStack.push_back(returnValue);
     }
-    // restore pointer
-    pointer = leaving->return_pointer;
-    ctxOffset[leaving->function->id()].pop_back();
-    frame = leaving->previous;
-    // restore bytecode
-    bc = frame->function->bytecode();
 }
--- students/2014/ovchinnikov/bytecodeinterpreter.hpp
+++ students/2014/ovchinnikov/bytecodeinterpreter.hpp
@@ -11,30 +11,35 @@ using std::map;
 
 using namespace mathvm;
 
+typedef union _v {
+    double d;
+    int64_t i;
+    uint16_t ui16;
+    _v() {}
+    _v(double d) : d(d) {}
+    _v(int64_t i) : i(i) {}
+    _v(uint16_t ui): ui16(ui) {}
+} Value;
+
+inline ostream & operator <<(ostream & out, const Value & v) {
+    return out << v.i;
+}
 
 class BytecodeInterpreter : public Code {
 
-    typedef union _v {
-        double d;
-        int64_t i;
-        uint16_t ui16;
-        _v() {}
-        _v(double d) : d(d) {}
-        _v(int64_t i) : i(i) {}
-        _v(uint16_t ui): ui16(ui) {}
-    } Value;
+
+
 
     struct StackFrame {
-        StackFrame *previous;
+        BytecodeFunction *function;
+        uint32_t pointer;
         uint32_t v_offset;
         uint32_t o_offset;
-        uint32_t return_pointer;
-        BytecodeFunction *function;
     };
 
 public:
     BytecodeInterpreter() {}
-    ~BytecodeInterpreter() {}
+    virtual ~BytecodeInterpreter() {}
     virtual Status *execute(vector<Var *> &);
 
 private:
@@ -48,17 +53,22 @@ private:
     }
 
     template<class T> T bc_read() {
-        T v = bc->getTyped<T>(pointer);
-        pointer += sizeof(T);
+        T v = bc()->getTyped<T>(pointer());
+        pointer() += sizeof(T);
         return v;
     }
+
+    Bytecode *bc() { return callStack.back().function->bytecode(); }
+    uint32_t & pointer() { return callStack.back().pointer; }
+    Instruction next() { return (Instruction)bc_read<uint8_t>(); }
+    Value & value(uint32_t varID) { return variableStack[callStack.back().v_offset + varID]; }
+    Value & value(uint32_t ctxID, uint32_t varID) { return variableStack[ctxOffset[ctxID].back() + varID]; }
 
     vector<Value> operandStack;
     vector<Value> variableStack;
     map<uint16_t, vector<uint32_t>> ctxOffset;
-    StackFrame *frame;
-    Bytecode *bc;
-    uint32_t pointer;
+    vector<StackFrame> callStack;
+
 };
 
 #endif // BYTECODEINTERPRETER_HPP
