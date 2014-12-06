bed7ef8e903de61ef803181220758a33bb000aaf
ovchinnikov.daniel@gmail.com <ovchinnikov.daniel@gmail.com@c972e689-a6e6-bf8a-7419-c86a4d87c60c>
2014-12-06 01:43:14 +0000

    additional functionality implemented

--- trunk/students/2014/ovchinnikov/blockscope.hpp
+++ trunk/students/2014/ovchinnikov/blockscope.hpp
@@ -18,16 +18,23 @@ using mathvm::Signature;
 using mathvm::AstVar;
 using mathvm::Var;
 using mathvm::VarType;
+using mathvm::BytecodeFunction;
 
 class BlockScope {
 
 public:
     BlockScope(BlockScope *parent)
-        : _functionId(parent->functionId()), _offset(parent->_offset + parent->_vars.size()), _parent(parent) {
+        : _parent(parent),
+          _function(parent->function()),
+          _offset(parent->_offset + parent->_vars.size()),
+          _locals(0) {
     }
 
-    BlockScope(uint16_t functionId, BlockScope *parent = 0, const Signature *signature = 0)
-        : _functionId(functionId), _offset(0), _parent(parent) {
+    BlockScope(BytecodeFunction *function, BlockScope *parent = 0, const Signature *signature = 0)
+        : _parent(parent),
+          _function(function),
+          _offset(0),
+          _locals(0) {
         if (!signature) { return; }
         for (auto it = signature->begin() + 1; it != signature->end(); ++it) {
             defineVar(it->first, it->second);
@@ -41,16 +48,19 @@ public:
     pair<uint16_t, uint16_t> resolveVar(const string & name) const;
 
     BlockScope *parent() const { return _parent; }
-    uint16_t functionId() const { return _functionId;}
+    BytecodeFunction *function() const { return _function;}
     uint16_t offset() const { return _offset; }
+    uint16_t size() const { return _vars.size(); }
+    uint16_t childLocals() const { return _locals; }
+    void setChildLocals(uint16_t locals) { _locals = locals > _locals ? locals : _locals; }
 
 private:
-    const uint16_t _functionId;
-    const uint16_t _offset;
     BlockScope *const _parent;
+    BytecodeFunction *const _function;
+    const uint16_t _offset;
     vector<Var> _vars;
     map<string, uint16_t> _nameToId;
-
+    uint16_t _locals;                   // number of locals in child blocks
 };
 
 #endif // BLOCKSCOPE_HPP
