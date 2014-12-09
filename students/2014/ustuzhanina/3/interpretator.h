4b01084b4998568a9995703e78c5ba42c8333c12
katejimme@gmail.com <katejimme@gmail.com@c972e689-a6e6-bf8a-7419-c86a4d87c60c>
2014-12-09 22:53:02 +0000

    little speed fix

--- students/2014/ustuzhanina/3/interpretator.h
+++ students/2014/ustuzhanina/3/interpretator.h
@@ -2,285 +2,320 @@
 #define INTERPRETATOR_H
 #include "mathvm.h"
 #include "context.h"
-#include "utils.h"
+
 #include <stack>
 #include "exceptions.h"
 using std::stack;
 using namespace mathvm;
 class InterpretCode : public Code
 {
-public:
-    InterpretCode(): position(0),
-        var0(VT_INT, "none"),
-        var1(VT_INT, "none")
-    {}
-    virtual Status * execute(vector<Var *> & vars)
-    {
-        Code::FunctionIterator it(this);
-        currenFunction = (BytecodeFunction *)it.next();
-        contextStack.push(currenFunction->id());
-        currentContext = createOrGetContext(1);
-        addContextToMap(1, currentContext);
+  public:
+	InterpretCode(): position(0),
+		var0(VT_INT, "none"),
+		var1(VT_INT, "none")
+	{}
 
-        //byteCode()->dump(cout);
-        try
-        {
-            handleByteCode();
-        }
-        catch(Exception ex)
-        {
-            return Status::Error(ex.what());
-        }
+	~InterpretCode()
+	{
+		for (auto it = contextMap.begin(); it != contextMap.end(); ++it)
+		{
+			auto val = it->second;
 
-        return Status::Ok();
-    }
+			for (int i = 0; i != val.size(); ++i)
+				delete val[i];
+		}
 
-    size_t getInstructionLength(Instruction instruction)
-    {
-        struct
-        {
-            Instruction insn;
-            size_t length;
-        } instructions[] =
-        {
+		//delete currentContext;
+	}
+	virtual Status * execute(vector<Var *> & vars)
+	{
+		Code::FunctionIterator it(this);
+		currenFunction = (BytecodeFunction *)it.next();
+		contextStack.push(currenFunction->id());
+		currentContext = createOrGetContext(1);
+		addContextToMap(1, currentContext);
+		initMap();
+
+		//byteCode()->dump(cout);
+		try
+		{
+			handleByteCode();
+		}
+		catch(Exception ex)
+		{
+			return Status::Error(ex.what());
+		}
+
+		return Status::Ok();
+	}
+
+	size_t getInstructionLength(Instruction instruction)
+	{
+		struct
+		{
+			Instruction insn;
+			size_t length;
+		} instructions[] =
+		{
 #define ENUM_ELEM_LEN(b, d, l) {BC_##b, l},
-            FOR_BYTECODES(ENUM_ELEM_LEN)
-        };
-        return instructions[instruction].length;
-    }
-private:
-    void handleByteCode();
-    BytecodeFunction * currenFunction;
-    Bytecode * byteCode()
-    {
-        return currenFunction->bytecode();
-    }
-    uint32_t position;
-    stack <Var> variables;
-    stack <uint32_t> contextStack;
-    Context * currentContext;
-    //create new context on every new call
-    map <int16_t, vector<Context *> > contextMap;
-    Var var0, var1;
+			FOR_BYTECODES(ENUM_ELEM_LEN)
+		};
+		return instructions[instruction].length;
+	}
+  private:
+	void handleByteCode();
+	BytecodeFunction * currenFunction;
+	Bytecode * byteCode()
+	{
+		return currenFunction->bytecode();
+	}
+	uint32_t position;
+	stack <Var> variables;
+	stack <uint32_t> contextStack;
+	Context * currentContext;
+	//create new context on every new call
+	map <int16_t, vector<Context *> > contextMap;
+	Var var0, var1;
 
-private:
-    VarType getType(Instruction instruction) const;
-    Var createVar(Instruction type, const string & name);
-    void pushDefaultVar(Instruction instruction);
-    void loadVar(Instruction instruction);
-    void saveVar(Instruction instruction);
-    void printVariable(Var var);
-    void swapValues();
-    void handleTopValue(Instruction instruction);
-    void handleAdditionVars(Instruction instruction);
-    void jump(int16_t position);
-    void cmpInstruction(Instruction instruction);
-    void logicalOperation(Instruction instruction);
-    Var getValueFromStack();
-    void handleCallNode(Instruction instruction);
-    void handleReturnNode();
+  private:
+	VarType getType(Instruction instruction) const;
+	Var createVar(Instruction type, const string & name);
+	void pushDefaultVar(Instruction instruction);
+	void loadVar(Instruction instruction);
+	void saveVar(Instruction instruction);
+	void printVariable(Var var);
+	void swapValues();
+	void handleTopValue(Instruction instruction);
+	void handleAdditionVars(Instruction instruction);
+	void jump(int16_t position);
+	void cmpInstruction(Instruction instruction);
+	void logicalOperation(Instruction instruction);
+	Var getValueFromStack();
+	void handleCallNode(Instruction instruction);
+	void handleReturnNode();
 
-private:
-    Context * createOrGetContext(int16_t idx)
-    {
-        Context::VariableMap vars;
-        Context::FunctionMapM funcs;
-        Context * context = new Context(idx, vars, funcs, NULL);
+  private:
+	Context * createOrGetContext(int16_t idx)
+	{
+		Context::VariableMap_idx vars;
+		Context::FunctionMapM funcs;
+		Context * context = new Context(idx, vars, funcs, NULL);
 
-        //create empty context if no one elems in Map
-        if (contextMap.find(idx) == contextMap.end()){
-            vector <Context *> contexts;
-            contextMap.insert(make_pair(idx, contexts));
-        }
-            //return contextMap.at(idx).front();
+		//create empty context if no one elems in Map
+		if (contextMap.find(idx) == contextMap.end())
+		{
+			vector <Context *> contexts;
+			contextMap.insert(make_pair(idx, contexts));
+		}
 
-        return context;
-    }
+		//return contextMap.at(idx).front();
+		return context;
+	}
 
-    void addContextToMap(int16_t key, Context *newContext){
-        contextMap.at(key).push_back(newContext);
-    }
+	void addContextToMap(int16_t key, Context * newContext)
+	{
+		contextMap.at(key).push_back(newContext);
+	}
 
-    Context * getContext(int16_t idx)
-    {
-        if (contextMap.find(idx) != contextMap.end())
-            return contextMap.at(idx).back();
-        else
-            throw Exception("try to load unexistence context");
-    }
+	Context * getContext(int16_t idx)
+	{
+		if (contextMap.find(idx) != contextMap.end())
+			return contextMap.at(idx).back();
+		else
+			throw Exception("try to load unexistence context");
+	}
 
-    vector<Context *> getContextMap(int16_t idx)
-    {
-        if (contextMap.find(idx) != contextMap.end())
-            return contextMap.at(idx);
-        else
-            throw Exception("try to load unexistence context Map");
-    }
+	vector<Context *> getContextMap(int16_t idx)
+	{
+		if (contextMap.find(idx) != contextMap.end())
+			return contextMap.at(idx);
+		else
+			throw Exception("try to load unexistence context Map");
+	}
 
-    Var getVariable(Context * context, int16_t idx)
-    {
-        if (context->variableMap.find(Utils::convertToString(idx)) != context->variableMap.end())
-            return context->variableMap.at(Utils::convertToString(idx)).second;
-        else
-            throw Exception("try to load unexistence variable "
-                            + Utils::convertToString(context->idx)  + " "
-                            + Utils::convertToString(idx)
-                            + Utils::convertToString(position));
-    }
+	Var getVariable(Context * context, int16_t idx)
+	{
+		if (context->varMap_idx.find(idx) != context->varMap_idx.end())
+			return context->varMap_idx.at(idx);
+		else
+			throw Exception("try to load unexistence variable ");
+	}
 
-    template <typename T>
-    void setValue(Var & var, T value)
-    {
-        switch (var.type())
-        {
-        case VT_INT:
-            var.setIntValue(value);
-            break;
+	template <typename T>
+	void setValue(Var & var, T value)
+	{
+		VarType type = var.type();
 
-        case VT_DOUBLE:
-            var.setDoubleValue((double)value);
-            break;
+		if (type == VT_INT)
+			var.setIntValue(value);
+		else
+			var.setDoubleValue((double)value);
+	}
 
-        default:
-            break;
-        }
-    }
+	template <typename T>
+	T getValue(Var & var)
+	{
+		VarType type = var.type();
 
-    template <typename T>
-    T getValue(Var & var)
-    {
-        switch (var.type())
-        {
-        case VT_INT:
-            return var.getIntValue();
-            break;
+		if (type == VT_INT)
+			return var.getIntValue();
+		else
+			return var.getDoubleValue();
 
-        case VT_DOUBLE:
-            return var.getDoubleValue();
-            break;
+		throw Exception("variable can be int, double or string type, but try to handle another");
+	}
 
-        case VT_STRING:
-            //            const char *val = var.getStringValue();
-            //            return val;
-            break;
+	void createDefaultVar(Instruction instruction)
+	{
+		Var var(getType(instruction), "default");
 
-        default:
-            break;
-        }
+		switch (instruction)
+		{
+		case BC_ILOAD0:
+			setValue<int64_t>(var, 0);
+			break;
 
-        throw Exception("variable can be int, double or string type, but try to handle another");
-    }
+		case BC_ILOAD1:
+			setValue<int64_t>(var, 1);
+			break;
 
-    void createDefaultVar(Instruction instruction)
-    {
-        Var var(Utils::getType(instruction), "default");
+		case BC_ILOADM1:
+			setValue<int64_t>(var, -1);
+			break;
 
-        switch (instruction)
-        {
-        case BC_ILOAD0:
-            setValue<int64_t>(var, 0);
-            break;
+		case BC_DLOAD0:
+			setValue<double>(var, 0);
+			break;
 
-        case BC_ILOAD1:
-            setValue<int64_t>(var, 1);
-            break;
+		case BC_DLOAD1:
+			setValue<double>(var, 1);
+			break;
 
-        case BC_ILOADM1:
-            setValue<int64_t>(var, -1);
-            break;
+		case BC_DLOADM1:
+			setValue<double>(var, -1);
+			break;
 
-        case BC_DLOAD0:
-            setValue<double>(var, 0);
-            break;
+		case BC_SLOAD0:
+			var.setStringValue("");
+			break;
 
-        case BC_DLOAD1:
-            setValue<double>(var, 1);
-            break;
+		default:
+			break;
+		}
 
-        case BC_DLOADM1:
-            setValue<double>(var, -1);
-            break;
+		variables.push(var);
+	}
 
-        case BC_SLOAD0:
-            var.setStringValue("");
-            break;
+	template <typename T>
+	void mathOperation(Instruction instruction)
+	{
+		T value1 = getValue<T>(variables.top());
+		Var result = variables.top();
+		variables.pop();
+		T value2 = getValue<T>(variables.top());
+		variables.pop();
 
-        default:
-            break;
-        }
+		switch (instruction)
+		{
+		case BC_DADD:
+		case BC_IADD:
+			setValue(result, value1 + value2);
+			break;
 
-        variables.push(var);
-    }
+		case BC_DSUB:
+		case BC_ISUB:
+			setValue(result, value1 - value2);
+			break;
 
-    template <typename T>
-    void mathOperation(Instruction instruction)
-    {
-        T value1 = getValue<T>(variables.top());
-        Var result = variables.top();
-        variables.pop();
-        T value2 = getValue<T>(variables.top());
-        variables.pop();
+		case BC_DMUL:
+		case BC_IMUL:
+			setValue(result, value1 * value2);
+			break;
 
-        switch (instruction)
-        {
-        case BC_DADD:
-        case BC_IADD:
-            setValue(result, value1 + value2);
-            break;
+		case BC_DDIV:
+		case BC_IDIV:
+			setValue(result, value1 / value2);
+			break;
 
-        case BC_DSUB:
-        case BC_ISUB:
-            setValue(result, value1 - value2);
-            break;
+		case BC_IMOD:
+			if (getType(instruction) == VT_INT)
+				setValue<int64_t>(result, (int64_t)value1 % (int64_t)value2);
 
-        case BC_DMUL:
-        case BC_IMUL:
-            setValue(result, value1 * value2);
-            break;
+			break;
 
-        case BC_DDIV:
-        case BC_IDIV:
-            setValue(result, value1 / value2);
-            break;
+		default:
+			break;
+		}
 
-        case BC_IMOD:
-            if (Utils::getType(instruction) == VT_INT)
-                setValue<int64_t>(result, (int64_t)value1 % (int64_t)value2);
-            break;
+		variables.push(result);
+	}
+	template <typename T>
+	void compareVar(Instruction instruction)
+	{
+		Var result(getType(instruction), "cmpres");
+		Var varUpper = getValueFromStack();
+		Var varLower = getValueFromStack();
+		T valueUpper  = getValue<T>(varUpper);
+		T valueLower = getValue<T>(varLower);
 
-        default:
-            break;
-        }
+		if (valueUpper > valueLower)
+		{
+			setValue<T>(result, 1);
+			variables.push(result);
+		}
+		else if (valueUpper == valueLower)
+		{
+			setValue<T>(result, 0);
+			variables.push(result);
+		}
+		else
+		{
+			setValue<T>(result, -1.0);
+			variables.push(result);
+		}
+	}
 
-        variables.push(result);
-    }
-    template <typename T>
-    void compareVar(Instruction instruction)
-    {
-        Var result(Utils::getType(instruction), "cmpres");
-        Var varUpper = variables.top();
-        variables.pop();
-        Var varLower = variables.top();
-        variables.pop();
-        T valueUpper  = getValue<T>(varUpper);
-        T valueLower = getValue<T>(varLower);
+	std::map <Instruction, VarType> typeMap;
+	void initMap()
+	{
+		typeMap.insert(std::make_pair(BC_ILOAD, VT_INT));
+		typeMap.insert(std::make_pair(BC_ILOAD0, VT_INT));
+		typeMap.insert(std::make_pair(BC_ILOAD1, VT_INT));
+		typeMap.insert(std::make_pair(BC_ILOADM1, VT_INT));
+		typeMap.insert(std::make_pair(BC_IADD, VT_INT));
+		typeMap.insert(std::make_pair(BC_ISUB, VT_INT));
+		typeMap.insert(std::make_pair(BC_IMUL, VT_INT));
+		typeMap.insert(std::make_pair(BC_IDIV, VT_INT));
+		typeMap.insert(std::make_pair(BC_IMOD, VT_INT));
+		typeMap.insert(std::make_pair(BC_INEG, VT_INT));
+		typeMap.insert(std::make_pair(BC_IAOR, VT_INT));
+		typeMap.insert(std::make_pair(BC_IAAND, VT_INT));
+		typeMap.insert(std::make_pair(BC_IAXOR, VT_INT));
+		typeMap.insert(std::make_pair(BC_I2D, VT_INT));
+		typeMap.insert(std::make_pair(BC_ICMP, VT_INT));
+		typeMap.insert(std::make_pair(BC_LOADCTXIVAR, VT_INT));
+		typeMap.insert(std::make_pair(BC_STORECTXIVAR, VT_INT));
+		typeMap.insert(std::make_pair(BC_DLOAD, VT_DOUBLE));
+		typeMap.insert(std::make_pair(BC_DLOAD0, VT_DOUBLE));
+		typeMap.insert(std::make_pair(BC_DLOAD1, VT_DOUBLE));
+		typeMap.insert(std::make_pair(BC_DLOADM1, VT_DOUBLE));
+		typeMap.insert(std::make_pair(BC_DADD, VT_DOUBLE));
+		typeMap.insert(std::make_pair(BC_DSUB, VT_DOUBLE));
+		typeMap.insert(std::make_pair(BC_DMUL, VT_DOUBLE));
+		typeMap.insert(std::make_pair(BC_DDIV, VT_DOUBLE));
+		typeMap.insert(std::make_pair(BC_DNEG, VT_DOUBLE));
+		typeMap.insert(std::make_pair(BC_D2I, VT_INT));
+		typeMap.insert(std::make_pair(BC_DCMP, VT_INT));
+		typeMap.insert(std::make_pair(BC_LOADCTXDVAR, VT_INT));
+		typeMap.insert(std::make_pair(BC_STORECTXDVAR, VT_INT));
+		typeMap.insert(std::make_pair(BC_SLOAD, VT_STRING));
+		typeMap.insert(std::make_pair(BC_SLOAD0, VT_STRING));
+	}
 
-        if (valueUpper > valueLower)
-        {
-            setValue<T>(result, 1);
-            variables.push(result);
-        }
-        else if (valueUpper == valueLower)
-        {
-            setValue<T>(result, 0);
-            variables.push(result);
-        }
-        else
-        {
-            setValue<T>(result, -1.0);
-            variables.push(result);
-        }
-    }
+	VarType getType(Instruction instruction)
+	{
+		return typeMap[instruction];
+	}
 };
 
 #endif // INTERPRETATOR_H
