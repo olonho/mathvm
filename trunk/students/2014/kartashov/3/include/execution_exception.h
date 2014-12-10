c6d459a1c8465538a26b092d33a6e2399d231fe6
snailandmail@gmail.com <snailandmail@gmail.com@c972e689-a6e6-bf8a-7419-c86a4d87c60c>
2014-12-10 10:35:48 +0000

    Moved exception to a separate folder

--- trunk/students/2014/kartashov/3/include/execution_exception.h
+++ trunk/students/2014/kartashov/3/include/execution_exception.h
@@ -8,9 +8,7 @@ class ExecutionException: public std::exception {
   public:
     ExecutionException(std::string message): mMessage(message) {}
 
-    const char* what() {
-      return mMessage.c_str();
-    }
+    const char* what() const noexcept { return mMessage.c_str();}
 
   private:
     const std::string mMessage;
