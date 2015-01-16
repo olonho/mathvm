bbe8db362e3d818db5f274994aa9531af6c026b7
inflamarse@gmail.com <inflamarse@gmail.com@c972e689-a6e6-bf8a-7419-c86a4d87c60c>
2015-01-16 02:18:53 +0000

    Added translator and interpreter.

---
+++ students/2014/goncharova/2/common.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "mathvm.h"
+#include <stdexcept>
+#include <cstdlib>
+#include <cstdio>
+
+typedef uint16_t idType;
+typedef std::pair<idType, idType> idPair;
+typedef int16_t offsetType;
