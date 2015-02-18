f96269a05c01de9c787627574372a92be66284cc
andronik.ordian@gmail.com <andronik.ordian@gmail.com@c972e689-a6e6-bf8a-7419-c86a4d87c60c>
2015-02-18 00:14:17 +0000

    toCpp11: native fix

--- trunk/students/2014/ordijan/toCpp11/CppCode.cpp
+++ trunk/students/2014/ordijan/toCpp11/CppCode.cpp
@@ -1,13 +1,19 @@
 #include "CppCode.h"
 #include <stdlib.h>
+#include <iostream>
 
 namespace mathvm {
 
 Status* CppCode::execute(vector<Var*>&) {
     string result = "/tmp/mathvm_tmp_result"; // tmpnam?
-    string callCmd = "g++ -std=c++11 -Ofast " + cppCodeFilename +
-                     " -o " + result + " && " + result;
+    string callCmd =
+        "g++ -std=c++11 -Ofast `sdl-config --cflags` -fPIC -D_POSIX_SOURCE -DMATHVM_WITH_SDL "
+        + cppCodeFilename +
+        " -o " + result +
+        " -ldl -rdynamic `sdl-config --libs` build/opt/utils.o";
+    system(("echo \"" + callCmd + "\"").c_str());
     system(callCmd.c_str());
+    system(result.c_str());
     return Status::Ok();
 }
 
--- trunk/students/2014/ordijan/toCpp11/visitor.cpp
+++ trunk/students/2014/ordijan/toCpp11/visitor.cpp
@@ -26,18 +26,86 @@ std::string escape(const std::string& s) {
 
 namespace mathvm {
 
-bool isNativeNode(FunctionNode* node) {
-    return node->body()->nodes() > 0
-        && node->body()->nodeAt(0)->isNativeCallNode();
+std::string typeToCppType(VarType type) {
+  switch (type) {
+  case VT_INT:    return "int64_t";
+  case VT_DOUBLE: return "double";
+  case VT_STRING: return "const char*";
+  case VT_VOID:   return "void";
+  default: assert(false);
+  }
 }
 
+bool isNativeNode(FunctionNode* node) {
+  return node->body()->nodes() > 0
+      && node->body()->nodeAt(0)->isNativeCallNode();
+}
 
+class NativeVisitor : public AstVisitor {
+  ostream &     _output;
+  const uint8_t _indentSpaces;
+
+  void indent() {
+    _output << string(_indentSpaces, ' ');
+  }
+
+  void visitFunctions(Scope* scope) {
+    Scope::FunctionIterator iter(scope);
+    while (iter.hasNext()) {
+      iter.next()->node()->visit(this);
+    }
+  }
+
+public:
+  NativeVisitor(ostream & output = std::cout,
+                const uint8_t indentSpaces = 2) :
+      _output(output), _indentSpaces(indentSpaces) {
+  }
+
+  virtual void visitFunctionNode(FunctionNode* node) {
+    if (!isNativeNode(node)) {
+      node->body()->visit(this);
+    } else {
+      indent();
+      _output << typeToCppType(node->returnType())
+              << " (* native_"
+              << node->body()->nodeAt(0)->asNativeCallNode()->nativeName();
+      _output << ")(";
+      for (uint32_t i = 0; i < node->parametersNumber(); i++) {
+        if (i != 0)
+            _output << ", ";
+        _output << typeToCppType(node->parameterType(i));
+      }
+      _output << ") = (";
+      _output << typeToCppType(node->returnType())
+              << " (*)(";
+      for (uint32_t i = 0; i < node->parametersNumber(); i++) {
+        if (i != 0)
+            _output << ", ";
+        _output << typeToCppType(node->parameterType(i));
+      }
+      _output << ")) dlsym(RTLD_DEFAULT, \""
+              << node->body()->nodeAt(0)->asNativeCallNode()->nativeName()
+              << "\");\n";
+      indent();
+      _output << "assert(native_"
+              << node->body()->nodeAt(0)->asNativeCallNode()->nativeName()
+              << ");\n";
+
+    }
+  }
+
+  virtual void visitBlockNode(BlockNode* node) {
+    visitFunctions(node->scope());
+  }
+};
 
 class ToCpp11Visitor : public AstVisitor {
   ostream &     _output;
   const uint8_t _indentSpaces; // avoid tabs
   uint16_t      _indentLevel;
   Scope*        _scope;
+  FunctionNode* _function;
 
   void indent() {
     _output << string(_indentSpaces * _indentLevel, ' ');
@@ -51,7 +119,7 @@ class ToCpp11Visitor : public AstVisitor {
     _indentLevel--;
   }
 
-  void variableDeclaration(Scope* scope) {
+  void variableDeclarations(Scope* scope) {
     Scope::VarIterator iter(scope);
     while (iter.hasNext()) {
       AstVar* x = iter.next();
@@ -61,26 +129,29 @@ class ToCpp11Visitor : public AstVisitor {
     }
   }
 
-  void functionDeclaration(Scope* scope) {
+  void functionDeclarations(Scope* scope) {
     Scope::FunctionIterator iter(scope);
     while (iter.hasNext()) {
-      indent();
       FunctionNode* node = iter.next()->node();
       if (isNativeNode(node))
-          continue;
+        continue;
+      indent();
+      functionSignature(node);
+      _output << "function_" << node->name() << ";\n";
+    }
+  }
 
-      if (node->name() != AstFunction::top_name) {
-          _output << "std::function<"
-                  << typeToCppType(node->returnType())
-                  << "(";
-          for (uint32_t i = 0; i < node->parametersNumber(); i++) {
-              if (i != 0)
-                  _output << ", ";
-              _output << typeToCppType(node->parameterType(i));
-          }
-          _output << ")> function_"
-                  << node->name() << ";\n";
+  void functionSignature(FunctionNode* node) {
+    if (node->name() != AstFunction::top_name) {
+      _output << "std::function<"
+              << typeToCppType(node->returnType())
+              << "(";
+      for (uint32_t i = 0; i < node->parametersNumber(); i++) {
+        if (i != 0)
+          _output << ", ";
+        _output << typeToCppType(node->parameterType(i));
       }
+      _output << ")> ";
     }
   }
 
@@ -93,20 +164,10 @@ class ToCpp11Visitor : public AstVisitor {
   }
 
   bool needSemicolon(AstNode* node) {
-      return node->isCallNode() || node->isBinaryOpNode()
-          || node->isUnaryOpNode() || node->isStringLiteralNode()
-          || node->isDoubleLiteralNode() || node->isIntLiteralNode()
-          || node->isLoadNode();
-  }
-
-  std::string typeToCppType(VarType type) const {
-    switch (type) {
-    case VT_INT:    return "int64_t";
-    case VT_DOUBLE: return "double";
-    case VT_STRING: return "const char*";
-    case VT_VOID:   return "void";
-    default: assert(false);
-    }
+    return node->isCallNode() || node->isBinaryOpNode()
+        || node->isUnaryOpNode() || node->isStringLiteralNode()
+        || node->isDoubleLiteralNode() || node->isIntLiteralNode()
+        || node->isLoadNode();
   }
 
  public:
@@ -119,13 +180,13 @@ class ToCpp11Visitor : public AstVisitor {
     Scope* oldscope = _scope;
     _scope = node->scope();
 
-    variableDeclaration(node->scope());
-    functionDeclaration(node->scope());
+    variableDeclarations(node->scope());
+    functionDeclarations(node->scope());
+    visitFunctions(node->scope());
 
     for (uint32_t i = 0; i < node->nodes(); i++) {
       indent();
       AstNode* current = node->nodeAt(i);
-      visitFunctions(node->scope());
       current->visit(this);
       if (needSemicolon(current))
         _output << ";";
@@ -217,46 +278,55 @@ class ToCpp11Visitor : public AstVisitor {
   }
 
   /**
-   *  int foo(int x) {...} -> std::function<int64_t(int64_t)> function_foo = [&](int x) -> int64_t {...};
+   *  int foo(int x) {...}
+   *     ==>
+   *  std::function<int64_t(int64_t)> function_foo;
+   *  function_foo = [&](int64_t x) -> int64_t {...};
    */
   virtual void visitFunctionNode(FunctionNode* node) {
-    if (isNativeNode(node))
-      return;
-
+    FunctionNode* old = _function;
+    _function = node;
     if (node->name() != AstFunction::top_name) {
+      if (!isNativeNode(node)) {
         _output << "function_"
                 << node->name() << " = [&]"
                 << "(";
-      for (uint32_t i = 0; i < node->parametersNumber(); i++) {
-        if (i != 0)
-          _output << ", ";
-        _output << typeToCppType(node->parameterType(i))
-                << " " << node->parameterName(i);
+        for (uint32_t i = 0; i < node->parametersNumber(); i++) {
+          if (i != 0)
+            _output << ", ";
+          _output << typeToCppType(node->parameterType(i))
+                  << " " << node->parameterName(i);
+        }
+        _output << ") -> " << typeToCppType(node->returnType());
+
+        node->body()->visit(this);
+        _output << ";" << endl;
       }
-      _output << ") -> " << typeToCppType(node->returnType());
-      node->body()->visit(this);
-      _output << ";" << endl;
     } else {
       // main
       _output <<
           "#include <cstdint>\n"
-          "#include <cmath>\n"
-          "#include <cstdio>\n"
-          "#include <cstdlib>\n"
+          "#include <cassert>\n"
+          "#include <dlfcn.h>\n"
           "#include <functional>\n"
           "#include <iostream>\n\n\n"
           "int main(void) {\n";
       blockEnter();
+      NativeVisitor native_visitor(_output, _indentSpaces);
+      node->body()->visit(&native_visitor);
       insideBlock(node->body());
       indent();
       blockExit();
       _output << "return 0;\n}\n";
     }
+    _function = old;
   }
 
   virtual void visitReturnNode(ReturnNode* node) {
     _output << "return";
-    if (node->returnExpr() != NULL) {
+    if (_function->name() == AstFunction::top_name)
+        _output << " 0";
+    else if (node->returnExpr() != NULL) {
       _output << " ";
       node->returnExpr()->visit(this);
     }
@@ -266,11 +336,13 @@ class ToCpp11Visitor : public AstVisitor {
   virtual void visitCallNode(CallNode* node) {
     AstFunction* f = _scope->lookupFunction(node->name());
     assert(f);
-    if (isNativeNode(f->node())) {
-      _output << f->node()->body()->nodeAt(0)->asNativeCallNode()->nativeName();
-    } else {
+    if (isNativeNode(f->node()))
+      _output << "(*native_"
+              << f->node()->body()->nodeAt(0)->asNativeCallNode()->nativeName()
+              << ")";
+    else
       _output << "function_" << node->name();
-    }
+
     // params
     _output << "(";
     for (uint32_t i = 0; i < node->parametersNumber(); i++) {
