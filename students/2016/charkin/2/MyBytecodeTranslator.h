080d6aa038bef6d50475db1ba32baf8127c1ac64
Kostya Charkin <93kostya@gmail.com>
2016-11-20 17:00:18 +0300

    add hw2 && hw3

---
+++ students/2016/charkin/2/MyBytecodeTranslator.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "mathvm.h"
+#include "visitors.h"
+#include "MyScope.h"
+
+namespace mathvm {
+    class MyBytecodeTranslator : public Translator {
+    public:
+        /**/virtual Status* translate(const string &program, Code **code);
+    };
+
+    class MyBytecodeVisitor : public AstVisitor {
+    public:
+        /**/MyBytecodeVisitor(Code* code);
+        /**/Status* translate(AstFunction* root);
+
+#define VISITOR_FUNCTION(type, name) \
+    /**/virtual void visit##type(type* node);
+
+    FOR_NODES(VISITOR_FUNCTION)
+#undef VISITOR_FUNCTION
+
+        /**/void process_function(AstFunction* ast_function, BytecodeFunction* bytecode_function);
+        /**/VarType process_logical_op(BinaryOpNode* node);
+        /**/VarType process_bitwise_op(BinaryOpNode* node);
+        /**/VarType process_comparasion_op(BinaryOpNode* node);
+        /**/VarType process_arithmetic_op(BinaryOpNode* node);
+
+    private:
+        Code* _code;
+        VarType _type;
+        MyScope* _scope;
+        /**/Bytecode* get_bytecode();
+        /**/void cast_type_to(VarType target);
+        /**/void store_var(const AstVar* var);
+        /**/void load_var(const AstVar* var);
+        /**/VarType get_common_type(VarType left_type, VarType right_type);
+
+    };
+}
