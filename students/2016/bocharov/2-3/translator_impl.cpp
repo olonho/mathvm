69f497eb8278fe98ee878bb48eb6b0bbbcaf9b8f
Fyodor Bocharov <bocharovfedor@gmail.com>
2016-11-20 13:26:06 +0300

    Implement translator (Bocharov)

---
+++ students/2016/bocharov/2-3/translator_impl.cpp
@@ -0,0 +1,43 @@
+#include "bytecode_generator.h"
+#include "interpretable_code.h"
+
+#include "mathvm.h"
+#include "parser.h"
+#include "visitors.h"
+
+#include <memory>
+
+namespace mathvm {
+
+Status* BytecodeTranslatorImpl::translate(const string& program, Code* *code)
+{
+    Parser parser;
+    std::unique_ptr<Status> status(parser.parseProgram(program));
+    if (status && status->isError()) {
+        return status.release();
+    }
+
+    *code = new InterpretableCode;
+    BytecodeGeneratorVisitor codegen(*code);
+    try {
+        parser.top()->node()->visit(&codegen);
+    } catch (const CodeGenerationError& e) {
+        return Status::Error(e.what(), e.position());
+    }
+
+    return Status::Ok();
+}
+
+Translator* Translator::create(const string& impl) {
+    if (impl == "" || impl == "intepreter") {
+        return new BytecodeTranslatorImpl();
+    }
+    if (impl == "jit") {
+        throw std::runtime_error("jit not implemented yet");
+        //return new MachCodeTranslatorImpl();
+    }
+    assert(false);
+    return 0;
+}
+
+}
