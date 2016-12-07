44df8ec6dce8066fa1dd84c2ed0aa91c91848604
Anton Mordberg <mordbergak@gmail.com>
2016-12-07 13:23:30 +0300

    updated project to fit tasks

--- students/2016/mordberg/main.cpp
+++ students/2016/mordberg/main.cpp
@@ -1,7 +1,7 @@
 #include "mathvm.h"
 
 #include <stdio.h>
-#include <fcntl.h>
+#include "translator_impl.h"
 #include <sys/stat.h>
 
 #include <iostream>
@@ -9,30 +9,32 @@
 using namespace mathvm;
 using namespace std;
 
-int main(int argc, char** argv) {
-    string impl = "";
-    const char* script = NULL;
+int main(int argc, char **argv) {
+    string impl = CMD_INTERPRETER;
+    const char *script = NULL;
     for (int32_t i = 1; i < argc; i++) {
-      if (string(argv[i]) == "-j") {
-        impl = "jit";
-      } else if (string(argv[i]) == "-p") {
-        impl = "printer";
-      } else if (string(argv[i]) == "-i") {
-        impl = "interpreter";
-      } else {
-        script = argv[i];
-      }
+        if (string(argv[i]) == "-j") {
+            impl = CMD_JIT;
+        } else if (string(argv[i]) == "-p") {
+            impl = CMD_PRINTER;
+        } else if (string(argv[i]) == "-t") {
+            impl = CMD_TRANSLATOR;
+        } else if (string(argv[i]) == "-i") {
+            impl = CMD_INTERPRETER;
+        } else {
+            script = argv[i];
+        }
     }
-    Translator* translator = Translator::create(impl);
+    Translator *translator = Translator::create(impl);
 
     if (translator == 0) {
         cout << "TODO: Implement translator factory in translator.cpp!!!!" << endl;
         return 1;
     }
 
-    const char* expr = "double x; double y;"
-                        "x += 8.0; y = 2.0;"
-                        "print('Hello, x=',x,' y=',y,'\n');";
+    const char *expr = "double x; double y;"
+            "x += 8.0; y = 2.0;"
+            "print('Hello, x=',x,' y=',y,'\n');";
     bool isDefaultExpr = true;
 
     if (script != NULL) {
@@ -44,50 +46,39 @@ int main(int argc, char** argv) {
         isDefaultExpr = false;
     }
 
-    Code* code = 0;
+    Code *code = 0;
 
-    Status* translateStatus = translator->translate(expr, &code);
+    Status *translateStatus = translator->translate(expr, &code);
     if (translateStatus->isError()) {
         uint32_t position = translateStatus->getPosition();
         uint32_t line = 0, offset = 0;
         positionToLineOffset(expr, position, line, offset);
         printf("Cannot translate expression: expression at %d,%d; "
-               "error '%s'\n",
+                       "error '%s'\n",
                line, offset,
                translateStatus->getErrorCstr());
     } else {
-        if (impl != "printer") {
-          assert(code != 0);
-          vector<Var*> vars;
-
-          if (isDefaultExpr) {
-            Var* xVar = new Var(VT_DOUBLE, "x");
-            Var* yVar = new Var(VT_DOUBLE, "y");
-            vars.push_back(xVar);
-            vars.push_back(yVar);
-            xVar->setDoubleValue(42.0);
-          }
-          Status* execStatus = code->execute(vars);
-          if (execStatus->isError()) {
-            printf("Cannot execute expression: error: %s\n",
-                   execStatus->getErrorCstr());
-          } else {
-            if (isDefaultExpr) {
-              printf("x evaluated to %f\n", vars[0]->getDoubleValue());
-              for (uint32_t i = 0; i < vars.size(); i++) {
-                delete vars[i];
-              }
+        if (impl == CMD_PRINTER) {
+            // nothing to do
+        } else if (impl == CMD_TRANSLATOR) {
+            code->disassemble();
+            delete code;
+        } else if (impl == CMD_INTERPRETER) {
+            auto args = vector<Var *>();
+            auto execStatus = code->execute(args);
+            if (execStatus->isError()) {
+                printf("Cannot execute expression: error: %s\n",
+                       execStatus->getErrorCstr());
             }
-          }
-          delete code;
-          delete execStatus;
+            delete code;
+            delete execStatus;
         }
     }
     delete translateStatus;
     delete translator;
 
     if (!isDefaultExpr) {
-      delete [] expr;
+        delete[] expr;
     }
 
     return 0;
--- students/2016/mordberg/translator_impl.cpp
+++ students/2016/mordberg/translator_impl.cpp
@@ -2,17 +2,23 @@
 
 #include "mathvm.h"
 #include "print_translator.h"
+#include "translator_impl.h"
 
 namespace mathvm {
 
+const std::string CMD_PRINTER = "printer";
+const std::string CMD_TRANSLATOR = "translator";
+const std::string CMD_INTERPRETER = "interpreter";
+const std::string CMD_JIT = "jit";
+
 Translator* Translator::create(const string& impl) {
-    if (impl == "printer") {
+    if (impl == CMD_PRINTER) {
         return new PrintTranslator(std::cout);
     } 
-    if (impl == "" || impl == "interpreter") {
+    if (impl == "" || impl == CMD_INTERPRETER || impl == CMD_TRANSLATOR) {
         return new BytecodeTranslatorImpl();
     }
-    if (impl == "jit") {
+    if (impl == CMD_JIT) {
         // return new MachCodeTranslatorImpl();
         return nullptr;
     }
