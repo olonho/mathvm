d52242a9a1faa79beb8a3a4a710774c7097e0571
simiyutin <simiyutin.boris@yandex.ru>
2017-09-29 19:59:23 +0300

    initial changes

---
+++ students/2017/simiyutin/1/ast_printer.cpp
@@ -0,0 +1,13 @@
+#include "include/ast_printer.h"
+
+using namespace mathvm;
+
+AstPrinterTranslatorImpl::AstPrinterTranslatorImpl() {}
+
+Status *AstPrinterTranslatorImpl::translate(const string &program, Code **code) {
+    return nullptr;
+}
+
+AstPrinterTranslatorImpl::~AstPrinterTranslatorImpl() {
+
+}
---
+++ students/2017/simiyutin/1/translator_impl.cpp
@@ -0,0 +1,21 @@
+#include "mathvm.h"
+#include "include/ast_printer.h"
+
+namespace mathvm {
+
+    Translator* Translator::create(const string& impl) {
+        if (impl == "printer") {
+            return new AstPrinterTranslatorImpl();
+        }
+        if (impl == "" || impl == "intepreter") {
+            //return new BytecodeTranslatorImpl();
+            return nullptr;
+        }
+        if (impl == "jit") {
+            //return new MachCodeTranslatorImpl();
+        }
+        assert(false);
+        return nullptr;
+    }
+
+}
