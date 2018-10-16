9dc9c97f206496bc0f86b712e7277a764368fa6a
vladislavkalinin <cripson1994@yandex.ru>
2018-10-16 05:14:13 +0300

    task1

---
+++ students/2018/kalinin/source_translator.h
@@ -0,0 +1,30 @@
+//
+// Created by Владислав Калинин on 16.10.2018.
+//
+
+#ifndef MATHVM_SOURCE_TRANSLATOR_H
+#define MATHVM_SOURCE_TRANSLATOR_H
+
+
+#include "../../../include/mathvm.h"
+#include "print_visitor.h"
+#include "../../../vm/parser.h"
+
+namespace mathvm {
+    class SourceTranslatorImpl : public Translator {
+    public:
+        Status *translate(const string &program, Code **code) override {
+            Parser p;
+            Status *status = p.parseProgram(program);
+            if (status->isOk()) {
+                AstFunction *astFunction = p.top();
+                auto *print_visitor = new Print_visitor();
+                astFunction->node()->visit(print_visitor);
+            }
+            return status;
+        }
+    };
+}
+
+
+#endif //MATHVM_SOURCE_TRANSLATOR_H
