591a6f7e856277b8897888fa70c5de0b61f4b91f
Dmitriy Novozhilov <novodmit.2x@yandex.ru>
2018-10-28 00:20:33 +0300

    [hw2] Novozhilov Dmitriy. Add second compiler pass to bytecode generation

--- students/2018/novozhilov/include/bytecode_generator.h
+++ students/2018/novozhilov/include/bytecode_generator.h
@@ -5,10 +5,17 @@
 #ifndef VIRTUAL_MACHINES_BYTECODE_INTERPRETER_H
 #define VIRTUAL_MACHINES_BYTECODE_INTERPRETER_H
 
+#include <ast.h>
 #include <visitors.h>
+#include <stack>
 #include "context.h"
 
 namespace mathvm {
+    struct AstInfo {
+        std::map<uint32_t, VarType> expressionType;
+        std::map<uint32_t, bool> returnValueUsed;
+    };
+
     class BytecodeGenerator : public AstBaseVisitor {
     public:
         explicit BytecodeGenerator(Code* code);
@@ -49,22 +56,21 @@ namespace mathvm {
 
         void visitReturnNode(ReturnNode *node) override;
 
-
     private:
+        class InfoCollector;
+
         Code *_code;
         Context *_context;
+        AstInfo _info;
+        BytecodeGenerator::InfoCollector *_infoCollector;
 
         Bytecode *getBytecode();
 
         void storeValueToVar(AstVar const *var);
 
-        void loadValueFromVar(AstVar const *var);
+        void loadValueFromVar(AstVar const *var, VarType targetType);
 
-        VarType getTypeOnStackTop();
-
-        void setTypeOnStackTop(VarType type);
-
-        void castVarOnStackTop(VarType type);
+        void castVarOnStackTop(VarType sourceType, VarType targetType);
 
         Context *findOwnerContextOfVar(string name);
 
@@ -72,7 +78,43 @@ namespace mathvm {
         void processLogicOperation(BinaryOpNode *node);
         void processComparingOperation(BinaryOpNode *node);
 
-        VarType processBinaryOperands(BinaryOpNode *node, bool castToInt = false);
+        VarType getNodeType(AstNode *node);
+
+    private:
+        class InfoCollector : public AstVisitor {
+        private:
+            BytecodeGenerator *_bytecodeGenerator;
+            std::stack<VarType> _returnTypes;
+
+        public:
+            explicit InfoCollector(BytecodeGenerator *bytecodeGenerator);
+
+            void visitForNode(ForNode *node) override;
+
+            void visitPrintNode(PrintNode *node) override;
+
+            void visitLoadNode(LoadNode *node) override;
+
+            void visitIfNode(IfNode *node) override;
+
+            void visitWhileNode(WhileNode *node) override;
+
+            void visitBlockNode(BlockNode *node) override;
+
+            void visitBinaryOpNode(BinaryOpNode *node) override;
+
+            void visitUnaryOpNode(UnaryOpNode *node) override;
+
+            void visitNativeCallNode(NativeCallNode *node) override;
+
+            void visitFunctionNode(FunctionNode *node) override;
+
+            void visitReturnNode(ReturnNode *node) override;
+
+            void visitStoreNode(StoreNode *node) override;
+
+            void visitCallNode(CallNode *node) override;
+        };
     };
 
 
