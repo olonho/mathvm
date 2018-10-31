35e598c95130c7fe8e9aa66762732bc0d135a6fc
Dmitriy Novozhilov <novodmit.2x@yandex.ru>
2018-10-31 11:09:12 +0300

    [hw3] Novozhilov Dmitriy. Add disabling tests in test system

--- tests/task3.py
+++ tests/task3.py
@@ -3,6 +3,12 @@
 import sys
 from common import *
 
+disabled_tests = {
+    'fib',
+    'fib_closure',
+    'ackermann',
+    'ackermann_closure',
+}
 
 def run_test(mvm, test_dir, test):
     try:
@@ -34,7 +40,12 @@ def run_test(mvm, test_dir, test):
 if __name__ == '__main__':
     passed_tests = []
     failed_tests = {}
+    actual_disabled_tests = set()
+
     for mvm, test_dir, test in load_tests():
+        if test in disabled_tests:
+            actual_disabled_tests.add(test)
+            continue
         passed, message = run_test(mvm, test_dir, test)
         if passed:
             passed_tests.append(test)
@@ -43,7 +54,13 @@ if __name__ == '__main__':
 
     print('\n')
 
-    if len(failed_tests) == 0:
+    if len(actual_disabled_tests) > 0:
+        print('DISABLED:')
+        for test in sorted(actual_disabled_tests):
+            print('Test: ' + test)
+        print('\n')
+
+    if len(failed_tests) == 0 and len(actual_disabled_tests) == 0:
         print('All tests passed')
         exit(0)
 
@@ -53,10 +70,9 @@ if __name__ == '__main__':
             print('Test: ' + test)
     if len(failed_tests) > 0:
         if len(passed_tests) > 0:
-            print()
-            print('------------------------------------------------')
+            print('\n')
         print('FAILED')
         for test in sorted(failed_tests.keys()):
-            print('---------------------------------')
             print('Test: ' + test)
             print(failed_tests[test])
+            print('---------------------------------')
